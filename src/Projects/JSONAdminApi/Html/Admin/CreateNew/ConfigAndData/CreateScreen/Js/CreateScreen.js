27ffe174783aad22b090baead5ab5599f58c9600
Srivasu215 <sri.vasu215@gmail.com>
2023-06-07 12:09:32 +0530

    vicky

---
+++ src/Projects/JSONAdminApi/Html/Admin/CreateNew/ConfigAndData/CreateScreen/Js/CreateScreen.js
@@ -0,0 +1,23 @@
+import { jFCreateFoldersToDom } from "./ShowOnDom.js";
+import ApiConfigJson from '../../' assert {type: 'json'};
+import { StartFunc as StartFuncAdminData } from "../../AdminData/StartFunc.js";
+import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
+import { StartFunc as StartFuncAfterDomLoad } from "./AfterDomLoad/StartFunc.js";
+import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom/StartFunc.js";
+
+let jVarCommonProjectName = ApiConfigJson.ProjectName;
+
+const StartFunc = () => {
+    let jVarLocalFromAdmin = StartFuncAdminData();
+
+    if (jVarLocalFromAdmin) {
+        StartFuncShowOnDom({ inProjectName: jVarCommonProjectName }).then(() => {
+            StartFuncFormLoad({ inProjectName: jVarCommonProjectName });
+
+            StartFuncAfterDomLoad({});
+        });
+    };
+};
+
+StartFunc();
+
