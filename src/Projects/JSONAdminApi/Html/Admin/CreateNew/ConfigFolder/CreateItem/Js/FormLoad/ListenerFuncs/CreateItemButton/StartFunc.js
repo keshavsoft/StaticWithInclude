bf3b34c97bd60585e00073466073948e7455b330
Sasi Kiran <sasikiran9515@gmail.com>
2023-06-07 13:18:37 +0530

    kanna

---
+++ src/Projects/JSONAdminApi/Html/Admin/CreateNew/ConfigFolder/CreateItem/Js/FormLoad/ListenerFuncs/CreateItemButton/StartFunc.js
@@ -0,0 +1,115 @@
+import { StartFunc as StartFuncPreparePostData } from "./PreparePostData.js";
+
+let StartFunc = async ({ inEvent, inProjectName }) => {
+    let localjFLocalCheckBeforeFetch = jFLocalCheckBeforeFetch({ inEvent });
+
+    if (localjFLocalCheckBeforeFetch) {
+        let jVarLocalBodyData = StartFuncPreparePostData({ inEvent });
+
+        let response = await jFLocalCallFetch({
+            inBodyData: jVarLocalBodyData,
+            inProjectName
+        });
+
+        jFLocalPostFetch({
+            inFromFetch: response,
+            inBodyData: jVarLocalBodyData
+        });
+    };
+};
+
+let jFLocalCheckBeforeFetch = ({ inEvent }) => {
+    let jVarLocalHtmlId = "CreateFileInputId";
+    let jVarCreateFolderInputId = document.getElementById(jVarLocalHtmlId);
+
+    let jVarLocalCurrentTarget = inEvent.currentTarget;
+    let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
+    let jVarLocalFileName = jVarLocalColsestTr.querySelector('[name="ItemName"]');
+    let jVarLocalFileNameValue = jVarLocalFileName.value;
+
+    if (jVarLocalFileNameValue === "") {
+        jVarLocalFileName.classList.add("is-invalid");
+        jVarLocalFileName.focus();
+        return false;
+    };
+
+    return true;
+};
+
+let jFLocalPostFetch = ({ inFromFetch, inBodyData }) => {
+    if (Array.isArray(inFromFetch)) {
+        jFLocalPostFetchAsArray({ inFromFetch, inBodyData });
+    };
+};
+
+let jFLocalPostFetchAsArray = ({ inFromFetch, inBodyData }) => {
+    if (jFLocalPostFetchCheckTF({ inFromFetch })) {
+        const myUrlWithParams = new URL(`${window.location.origin}${window.location.pathname}`);
+
+        if ("FolderName" in inBodyData) {
+            myUrlWithParams.searchParams.append("inFolderName", inBodyData.FolderName);
+        };
+
+        if ("FileName" in inBodyData) {
+            myUrlWithParams.searchParams.append("inFileName", inBodyData.FileName);
+        };
+
+        if ("NewItemName" in inBodyData) {
+            myUrlWithParams.searchParams.append("NewItemName", inBodyData.NewItemName);
+        };
+
+        window.location.href = myUrlWithParams.href;
+    };
+};
+
+let jFLocalPostFetchCheckTF = ({ inFromFetch }) => {
+    const myUrlWithParams = new URL(window.location.href);
+
+    let jVarLocalFromConfig = inFromFetch.find(element => {
+        return element.ConfigFolderCreated
+    });
+
+    if (jVarLocalFromConfig === undefined === false) {
+        myUrlWithParams.searchParams.append("ConfigFolderCreated", true);
+    };
+
+    let jVarLocalFromData = inFromFetch.find(element => {
+        return element.DataFolderCreated
+    });
+
+    if (jVarLocalFromData === undefined === false) {
+        myUrlWithParams.searchParams.append("DataFolderCreated", true);
+    };
+
+    let jVarLocalAnyCreated = inFromFetch.filter(element => {
+        return element.KTF
+    });
+
+    if (jVarLocalAnyCreated.length > 0) {
+        return true;
+    };
+
+    return false;
+};
+
+
+let jFLocalCallFetch = async ({ inBodyData, inProjectName }) => {
+    let jFetchUrl = `/${inProjectName}/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/CreateNew/CreateItem`;
+
+    let jFetchBody = {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inBodyData)
+    };
+
+    let response = await fetch(jFetchUrl, jFetchBody);
+
+    if (response.status === 200) {
+        return await response.json();
+    };
+};
+
+export { StartFunc };
