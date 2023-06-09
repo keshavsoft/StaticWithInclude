da64be3a13af074afb898cd680c1bbefe54d7294
JatinNeeraj <jatinucm123@gmail.com>
2023-06-09 16:44:19 +0530

    jatin1

--- src/Projects/JSONAdminApi/Html/Admin/CreateNew/DataFolder/CreateItem/Js/ShowOnDom/ApplyClasses.js
+++ src/Projects/JSONAdminApi/Html/Admin/CreateNew/DataFolder/CreateItem/Js/ShowOnDom/ApplyClasses.js
@@ -36,30 +36,4 @@ let getUrlQueryParams = ({ inGetKey }) => {
     return value;
 };
 
-let StartFunc11111 = ({ inDataFromApi }) => {
-    let jVarLocalFromUrl = getUrlQueryParams({ inGetKey: "inFolderName" });
-    let jVarLocalFromUrlinFileName = getUrlQueryParams({ inGetKey: "inFileName" });
-    console.log("jVarLocalFromUrl : ", jVarLocalFromUrl);
-
-    if (jVarLocalFromUrl === null === false) {
-        let jVarLocalFolderkeyNeeded = `Folders.${jVarLocalFromUrl}.Files.${jVarLocalFromUrlinFileName}`;
-
-        _.has(inDataFromApi, `Folders.${jVarLocalFromUrl}`) && _.set(inDataFromApi, `Folders.${jVarLocalFromUrl}.TabPageClass`, " show active");
-        _.has(inDataFromApi, `Folders.${jVarLocalFromUrl}`) && _.set(inDataFromApi, `Folders.${jVarLocalFromUrl}.MenuClass`, " active");
-
-        if (jVarLocalFromUrlinFileName === null === false) {
-            let jVarLocalFilekeyNeeded = `Folders.${jVarLocalFromUrl}.Files.${jVarLocalFromUrlinFileName}`;
-
-            _.has(inDataFromApi, jVarLocalFilekeyNeeded) && _.set(inDataFromApi, `${jVarLocalFilekeyNeeded}.TabPaneClass`, " show active");
-            _.has(inDataFromApi, jVarLocalFilekeyNeeded) && _.set(inDataFromApi, `${jVarLocalFilekeyNeeded}.ButtonClass`, " active");
-        };
-    };
-
-    // _.set(inDataFromApi, `Folders.${jVarLocalFromUrl}.MenuClass`, " active");
-
-    // _.set(inDataFromApi, `Folders.${jVarLocalFromUrl}.Files.${jVarLocalFromUrlinFileName}.TabPaneClass`, " show active");
-    // _.set(inDataFromApi, `Folders.${jVarLocalFromUrl}.Files.${jVarLocalFromUrlinFileName}.ButtonClass`, " active");
-};
-
-
 export { StartFunc };
