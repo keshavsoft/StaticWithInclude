dea785872b8de2d417461bac6bb856a1ff3bdb4b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-13 17:35:14 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Html/Admin/Admin/Preload/Duplicate/Js/FormLoad/Addlisteners.js
+++ src/Projects/JSONAdminApi/Html/Admin/Admin/Preload/Duplicate/Js/FormLoad/Addlisteners.js
@@ -1,7 +1,7 @@
 import { StartFunc as StartFuncListenerFuncs } from "./ListenerFuncs/StartFunc.js";
 
 let StartFunc = ({ inProjectName }) => {
-    let jVarLocalFileCreateClassName = document.getElementsByClassName("CreateColumnButtonClass");
+    let jVarLocalFileCreateClassName = document.getElementsByClassName("CloneButtonClass");
 
     for (let i = 0; i < jVarLocalFileCreateClassName.length; i++) {
         jVarLocalFileCreateClassName[i].addEventListener("click", async (event) => {
