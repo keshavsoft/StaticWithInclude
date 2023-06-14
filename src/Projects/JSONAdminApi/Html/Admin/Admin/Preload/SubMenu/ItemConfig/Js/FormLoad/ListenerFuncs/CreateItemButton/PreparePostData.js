4aba8cc7464024debc09ba8ec0cbe8e03f4c2cbb
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-14 20:02:46 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Html/Admin/Admin/Preload/SubMenu/ItemConfig/Js/FormLoad/ListenerFuncs/CreateItemButton/PreparePostData.js
+++ src/Projects/JSONAdminApi/Html/Admin/Admin/Preload/SubMenu/ItemConfig/Js/FormLoad/ListenerFuncs/CreateItemButton/PreparePostData.js
@@ -1,29 +1,21 @@
 let StartFunc = ({ inEvent }) => {
     let jVarLocalCurrentTarget = inEvent.currentTarget;
+    let jVarLocalfromkey = jVarLocalCurrentTarget.dataset.fromkey;
+
     let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
-    let jVarLocalColumnName = jVarLocalColsestTr.querySelector('[name="ColumnName"]');
-    let jVarLocalColumnNameValue = jVarLocalColumnName.value;
+    let jVarLocalItemName = jVarLocalColsestTr.querySelector('[name="ItemName"]');
 
-    let jVarLocalFromButtonData = jFLocalFromButton({ inCurrentTarget: jVarLocalCurrentTarget });
+    let jVarLocalItemNameValue = jVarLocalItemName.value;
 
-    return {
-        ...jVarLocalFromButtonData,
-        NewColumnName: jVarLocalColumnNameValue
+    let localBodyAsJson = {
+        ItemName: jVarLocalItemNameValue,
     };
-};
 
-const jFLocalFromButton = ({ inCurrentTarget }) => {
-    let jVarLocalCurrentTarget = inCurrentTarget;
-    let jVarLocalFolderName = jVarLocalCurrentTarget.dataset.foldername;
-    let jVarLocalFileName = jVarLocalCurrentTarget.dataset.filename;
-    let jVarLocalitemname = jVarLocalCurrentTarget.dataset.itemname;
-    let jVarLocalscreenname = jVarLocalCurrentTarget.dataset.screenname;
 
     return {
-        FolderName: jVarLocalFolderName,
-        FileName: jVarLocalFileName,
-        ItemName: jVarLocalitemname,
-        ScreenName: jVarLocalscreenname
+        KeyName: jVarLocalfromkey,
+        BodyAsJson: localBodyAsJson
+
     };
 };
 
