c45d905741a9dd524b88094eec21f8db4246a762
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-15 18:11:39 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Html/Admin/SubTableColumns/Tabular/Alter/SubMenu/KDataset.js
+++ src/Projects/JSONAdminApi/Html/Admin/SubTableColumns/Tabular/Alter/SubMenu/KDataset.js
@@ -1,17 +1,17 @@
 import { jFUpdateFunc } from "./KDataset/Addlisteners.js";
 import { jFCreateFoldersToDom } from "./KDataset/ShowOnDom.js";
-import { AdminDataStartFunc } from "../../../../../AdminData/StartFunc.js";
+import { StartFunc as StartFuncAdminData } from "../../../../../AdminData/StartFunc.js";
 import ApiConfig from "../../../../ApiConfig.json" assert{type: "json"};
 
 let commonProjectName = ApiConfig.ProjectName;
 
 let StartFunc = () => {
-    let localAdminDataStartFunc = AdminDataStartFunc();
+    let localAdminDataStartFunc = StartFuncAdminData();
 
     if (localAdminDataStartFunc) {
         jFCreateFoldersToDom({ inProjectName: commonProjectName }).then(FromjFCreateFoldersToDom => {
 
-            jFUpdateFunc({inProjectName});
+            jFUpdateFunc({ inProjectName: commonProjectName });
         });
     }
 };
