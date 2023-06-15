9eda8fa792163d1f81d472ef3dda18164b62d645
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-15 13:34:12 +0530

    SREENIVAS

---
+++ src/Projects/JSONAdminApi/Html/Admin/Admin/Preload/SubMenu/DataListInfo/Js/FormLoad/ListenerFuncs/CreateItemButton/StartFunc.js
@@ -0,0 +1,41 @@
+import { StartFunc as StartFuncPreparePostData } from "./PreparePostData.js";
+import { StartFunc as StartFuncPostFetch } from "./PostFetch.js";
+import { StartFunc as StartFuncFetchFunc } from "./FetchFunc.js";
+
+let StartFunc = async ({ inEvent, inProjectName }) => {
+
+    let jFetchUrl = `/${inProjectName}/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/DataListInfoKey`;
+
+    let jVarLocalBodyData = StartFuncPreparePostData({ inEvent });
+
+    let response = await StartFuncFetchFunc({
+        inBodyData: jVarLocalBodyData,
+        Url: jFetchUrl
+    });
+
+    StartFuncPostFetch({
+        inFromFetch: response,
+        inBodyData: jVarLocalBodyData
+    });
+};
+
+// let jFLocalCallFetch = async ({ inBodyData, Url }) => {
+//     let localUrl = Url;
+
+//     let jFetchBody = {
+//         method: "post",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(inBodyData)
+//     };
+
+//     let response = await fetch(localUrl, jFetchBody);
+
+//     if (response.status === 200) {
+//         return await response.json();
+//     };
+// };
+
+export { StartFunc };
