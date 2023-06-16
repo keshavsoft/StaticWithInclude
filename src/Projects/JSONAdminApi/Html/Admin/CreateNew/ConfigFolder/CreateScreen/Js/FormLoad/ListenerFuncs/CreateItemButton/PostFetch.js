7ca223506567b7e1a132e8e40fce246f089576f1
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-16 19:27:29 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Html/Admin/CreateNew/ConfigFolder/CreateScreen/Js/FormLoad/ListenerFuncs/CreateItemButton/PostFetch.js
+++ src/Projects/JSONAdminApi/Html/Admin/CreateNew/ConfigFolder/CreateScreen/Js/FormLoad/ListenerFuncs/CreateItemButton/PostFetch.js
@@ -1,57 +1,23 @@
-let StartFunc = ({ inFromFetch, inBodyData }) => {
-    if (Array.isArray(inFromFetch)) {
-        jFLocalPostFetchAsArray({ inFromFetch, inBodyData });
-    };
+let StartFunc = ({ inFromFetch, inBodyData, CurrentTarget }) => {
+    if ("KTF" in inFromFetch) {
+        if (inFromFetch.KTF) {
+            window.location = "";
+
+        } else {
+            let jVarLocalClosestTr = CurrentTarget.closest("tr");
+            let jVarLocalInput = jVarLocalClosestTr.querySelector('[name="ScreenName"]');
+            let jVarLocaldiv = jVarLocalClosestTr.querySelector('.invalid-feedback');
+
+            jVarLocalInput.classList.add("is-invalid");
+            jVarLocaldiv.innerHTML = "Column: already found !"
+
+        }
+
+    } else {
+        console.log("KTF not found in fetch");
+    }
+
 };
 
-let jFLocalPostFetchCheckTF = ({ inFromFetch }) => {
-    const myUrlWithParams = new URL(window.location.href);
-
-    let jVarLocalFromConfig = inFromFetch.find(element => {
-        return element.ConfigFolderCreated
-    });
-
-    if (jVarLocalFromConfig === undefined === false) {
-        myUrlWithParams.searchParams.append("ConfigFolderCreated", true);
-    };
-
-    let jVarLocalFromData = inFromFetch.find(element => {
-        return element.DataFolderCreated
-    });
-
-    if (jVarLocalFromData === undefined === false) {
-        myUrlWithParams.searchParams.append("DataFolderCreated", true);
-    };
-
-    let jVarLocalAnyCreated = inFromFetch.filter(element => {
-        return element.KTF
-    });
-
-    if (jVarLocalAnyCreated.length > 0) {
-        return true;
-    };
-
-    return false;
-};
-
-let jFLocalPostFetchAsArray = ({ inFromFetch, inBodyData }) => {
-    if (jFLocalPostFetchCheckTF({ inFromFetch })) {
-        const myUrlWithParams = new URL(`${window.location.origin}${window.location.pathname}`);
-
-        if ("FolderName" in inBodyData) {
-            myUrlWithParams.searchParams.append("inFolderName", inBodyData.FolderName);
-        };
-
-        if ("FileName" in inBodyData) {
-            myUrlWithParams.searchParams.append("inFileName", inBodyData.FileName);
-        };
-
-        if ("ItemName" in inBodyData) {
-            myUrlWithParams.searchParams.append("inItemName", inBodyData.ItemName);
-        };
-
-        window.location.href = myUrlWithParams.href;
-    };
-};
 
 export { StartFunc };
