db24ba24d62240e06a7a6063aeeda8c76239fb7f
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-19 13:32:53 +0530

    keshav

--- src/Projects/JSONApi/Html/UserData/ForCrud/Vertical.js
+++ src/Projects/JSONApi/Html/UserData/ForCrud/Vertical.js
@@ -1,21 +1,16 @@
 import { jFShowFoldersInMenu } from "../ForDataHtml/Header/FromFolder.js";
-import { StartFunc as VerticalStartFunc } from "./Show/FetchFuns.js";
+import { StartFunc as VerticalStartFunc } from "./Vertical/FetchFuns.js";
 //import { StartFunc as KeyPressStartFunc } from "./Vertical/AddListenersFuncs/KeyPress.js";
-import { StartFunc as CardHeaderStartFunc } from "./Show/AddListenersFuncs/Card/CardHeader/ButtonClicks.js";
-import { StartFunc as AddListenersFuncsStartFunc } from "./Show/AddListenersFuncs/StartFunc.js";
+import { StartFunc as AddListenersFuncsStartFunc } from "./Vertical/AddListenersFuncs/StartFunc.js";
+import { StartFunc as CardHeaderStartFunc } from "./Vertical/AddListenersFuncs/Card/CardHeader/ButtonClicks.js";
+import { StartFunc as StartFuncClickFuncs } from "../ForDataHtml/Header/ClickFuncs.js";
+
+StartFuncClickFuncs();
 
 jFShowFoldersInMenu({
     inProjectName: jVarGlobalProject,
     inSubRoute: jVarGlobalSubRoute
 });
-
-let jFLocalForFocus = () => {
-    let jVarLocalFirstInput = document.querySelector("table tfoot input");
-
-    if (jVarLocalFirstInput === null === false) {
-        jVarLocalFirstInput.focus();
-    };
-};
 
 VerticalStartFunc({
     inProjectName: jVarGlobalProject,
@@ -27,7 +22,5 @@ VerticalStartFunc({
             inProjectName: jVarGlobalProject,
             inSubRoute: jVarGlobalSubRoute
         });
-
-        jFLocalForFocus();
     };
 });
