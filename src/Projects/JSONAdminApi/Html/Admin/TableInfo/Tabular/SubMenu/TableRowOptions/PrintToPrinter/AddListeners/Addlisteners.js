fbfeaf659ca5f373bf4cd756a48713fe0207857b
JatinNeeraj <jatinucm123@gmail.com>
2023-06-28 18:30:20 +0530

    Neeraj28

---
+++ src/Projects/JSONAdminApi/Html/Admin/TableInfo/Tabular/SubMenu/TableRowOptions/PrintToPrinter/AddListeners/Addlisteners.js
@@ -0,0 +1,71 @@
+import { StartFunc as StartFuncUpdateButton } from "./UpdateButton/ClickFunc.js";
+
+let jFUpdateFunc = () => {
+    let jVarLocalUpdateClassName = document.getElementsByClassName("UpdateButtonClass");
+
+    for (let i = 0; i < jVarLocalUpdateClassName.length; i++) {
+        jVarLocalUpdateClassName[i].addEventListener("click", StartFuncUpdateButton)
+    };
+};
+
+let jFLocalClickFunc = async (event) => {
+    let jVarLocalCurrentTarget = event.currentTarget;
+    let jVarLocalFolderName = jVarLocalCurrentTarget.dataset.foldername;
+    let jVarLocalfilename = jVarLocalCurrentTarget.dataset.filename;
+    let jVarLocalitemname = jVarLocalCurrentTarget.dataset.itemname;
+    let jVarLocalscreenname = jVarLocalCurrentTarget.dataset.screenname;
+
+    let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
+    let jVarLocalDouble = jVarLocalColsestTr.querySelector('[name="Double"]');
+    let jVarLocalBarcode = jVarLocalColsestTr.querySelector('[name="Barcode"]');
+
+    let jVarLocalDoubleValue = jVarLocalDouble.checked;
+    let jVarLocalBarcodeValue = jVarLocalBarcode.checked;
+
+    let BodyAsJson = {
+        Double: jVarLocalDoubleValue,
+        Barcode: jVarLocalBarcodeValue
+    }
+
+    let jFetchUrl = "/JSONAdminApi/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/PrintToPrinter";
+
+    let FetchBody = {
+        method: "PATCH",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            folderName: jVarLocalFolderName,
+            FileName: jVarLocalfilename,
+            ItemName: jVarLocalitemname,
+            ScreenName: jVarLocalscreenname,
+            BodyAsJson
+        })
+    }
+
+    let response = await fetch(jFetchUrl, FetchBody);
+
+    switch (response.status) {
+        case 200:
+            //window.location = "";
+            let jVarLocalNewLocation = "";
+            jVarLocalNewLocation += `?inFolderName=${jVarLocalFolderName}`
+            jVarLocalNewLocation += `&inFileName=${jVarLocalfilename}`
+            jVarLocalNewLocation += `&inItemName=${jVarLocalitemname}`
+            jVarLocalNewLocation += `&inScreenName=${jVarLocalscreenname}`;
+            window.location = jVarLocalNewLocation;
+
+            break;
+
+        case 204:
+            Swal.fire('Not Update Data')
+            break;
+        default:
+        // code block
+    };
+
+    console.log("response : ", response.status);
+};
+
+export { jFUpdateFunc };
