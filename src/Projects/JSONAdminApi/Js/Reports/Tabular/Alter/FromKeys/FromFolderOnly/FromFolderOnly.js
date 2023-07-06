9bec708ffd2c21132a1d060fc9939dbaa535dbde
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-06 13:04:37 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/FromKeys/FromFolderOnly/FromFolderOnly.js
+++ src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/FromKeys/FromFolderOnly/FromFolderOnly.js
@@ -1,6 +1,8 @@
 import { jFUpdateFunc as jFUpdateFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
 import { jFCreateFoldersToDom } from "./ShowOnDom/ShowOnDom.js";
+import { StartFunc as StartFuncServer } from "./FetchCalls/Server.js";
 
 jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
+    StartFuncServer();
     jFUpdateFuncAddlisteners();
 });
