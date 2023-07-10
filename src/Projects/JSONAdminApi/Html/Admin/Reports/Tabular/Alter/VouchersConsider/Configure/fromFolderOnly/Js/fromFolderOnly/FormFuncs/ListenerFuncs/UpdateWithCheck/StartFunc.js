49d72fd132087db4e6995969f127fad22bed271c
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-07-10 13:06:30 +0530

    ramp2403

---
+++ src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/Configure/fromFolderOnly/Js/fromFolderOnly/FormFuncs/ListenerFuncs/UpdateWithCheck/StartFunc.js
@@ -0,0 +1,53 @@
+let StartFunc = async ({ inevent }) => {
+    let jVarLocalCurrentTarget = inevent.currentTarget;
+    let jVarLocalItemName = jVarLocalCurrentTarget.dataset.item;
+
+    let jVarLocalvoucherName = jVarLocalCurrentTarget.dataset.voucher;
+
+    let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
+    let jVarLocalFolderName = jVarLocalColsestTr.querySelector('[name="FolderName"]');
+    let jVarLocalFromFolder = jVarLocalColsestTr.querySelector('[name="FromFolder"]');
+
+    let jVarLocalFolderNameValue = jVarLocalFolderName.value;
+    let jVarLocalFromFolderValue = jVarLocalFromFolder.checked;
+
+    let BodyAsJson = {
+        FolderName: jVarLocalFolderNameValue,
+        FromFolder: jVarLocalFromFolderValue
+        
+    };
+
+    let jFetchUrl = "/JSONAdminApi/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys/FromFolderOnly";
+    let jVarLocalRequestHeader = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+             ItemName: jVarLocalItemName,
+            voucher: jVarLocalvoucherName,
+            BodyAsJson
+        })
+    }
+
+    let response = await fetch(jFetchUrl, jVarLocalRequestHeader);
+
+   switch (responseData.status) {
+        case 200:
+            let jVarLocalNewLocation = "";
+            jVarLocalNewLocation += `?inReportName=${jVarLocalItemName}`
+            jVarLocalNewLocation += `&inRowPK=${jVarLocalvoucherName}`
+            window.location = jVarLocalNewLocation;
+
+            break;
+
+        case 204:
+            Swal.fire('Not Update Data')
+            break;
+        default:
+        // code block
+    };
+};
+
+export { StartFunc };
--- src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/Configure/fromFolderOnly/Js/fromFolderOnly/fromFolderOnly.js
+++ src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/Configure/fromFolderOnly/Js/fromFolderOnly/fromFolderOnly.js
@@ -1,8 +1,11 @@
 import { jFUpdateFunc as jFUpdateFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
 import { jFCreateFoldersToDom } from "./ShowOnDom/ShowOnDom.js";
 import { StartFunc as StartFuncServer } from "./FetchCalls/Server.js";
+import { StartFunc as StartFuncFormFuncs } from "./FormFuncs/StartFunc.js";
+
 
 jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
     StartFuncServer();
-    jFUpdateFuncAddlisteners();
+    //jFUpdateFuncAddlisteners();
+    StartFuncFormFuncs();
 });
