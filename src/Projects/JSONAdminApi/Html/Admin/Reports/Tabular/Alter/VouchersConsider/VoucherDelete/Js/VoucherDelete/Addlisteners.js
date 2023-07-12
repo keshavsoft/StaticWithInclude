3394021d288efefc20e4d67ae591311104cd70fb
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-12 19:12:55 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/VoucherDelete/Js/VoucherDelete/Addlisteners.js
+++ src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/VoucherDelete/Js/VoucherDelete/Addlisteners.js
@@ -1,9 +1,7 @@
 import { StartFunc as StartFuncDeleteButton } from "./DeleteButton/1-ClickFunc.js";
-import { StartFunc as StartFuncDeleteWithCheckButton } from "./DeleteWithCheckButton/1-ClickFunc.js";
 
 const StartFunc = () => {
     StartFuncDeleteButton();
-    StartFuncDeleteWithCheckButton();
 };
 
 export { StartFunc };
