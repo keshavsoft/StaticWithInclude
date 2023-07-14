6f2cbf0b98438405707985f237e6bc0f2ec23bcd
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-14 18:24:39 +0530

    neeraj2

---
+++ src/Projects/JSONAdminApi/Html/Admin/Columns/Tabular/Alter/SubMenu/KDataAttributes/AddListeners/6-AfterFetch.js
@@ -0,0 +1,27 @@
+let StartFunc = ({ inFromFetch }) => {
+    let response = inFromFetch;
+    switch (response.status) {
+        case 200:
+            //window.location = "";
+            let jVarLocalNewLocation = "";
+            jVarLocalNewLocation += `?inFolderName=${jVarLocalFolderName}`
+            jVarLocalNewLocation += `&inFileName=${jVarLocalfilename}`
+            jVarLocalNewLocation += `&inItemName=${jVarLocalitemname}`
+            jVarLocalNewLocation += `&inScreenName=${jVarLocalscreenname}`
+            jVarLocalNewLocation += `&inColumnName=${jVarLocalDataAttributeValue}`;
+            console.log("jVarLocalNewLocation : ", jVarLocalNewLocation);
+            window.location = jVarLocalNewLocation;
+
+            break;
+
+        case 204:
+            Swal.fire('Not Update Data')
+            break;
+        default:
+        // code block
+    };
+
+    console.log("response : ", response.status);
+};
+
+export { StartFunc };
