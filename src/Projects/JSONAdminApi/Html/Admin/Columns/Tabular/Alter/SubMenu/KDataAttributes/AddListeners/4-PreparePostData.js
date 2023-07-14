416a829c0f1c0c0f56aec63881be3c0a81149d1f
Satish Addanki <satishaddanki321@gmail.com>
2023-07-14 20:31:16 +0530

    satish2

--- src/Projects/JSONAdminApi/Html/Admin/Columns/Tabular/Alter/SubMenu/KDataAttributes/AddListeners/4-PreparePostData.js
+++ src/Projects/JSONAdminApi/Html/Admin/Columns/Tabular/Alter/SubMenu/KDataAttributes/AddListeners/4-PreparePostData.js
@@ -1,16 +1,30 @@
-let StartFunc = () => { 
+let StartFunc = () => {
     let jVarLocalCurrentTarget = event.currentTarget;
     let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
+    let jVarLocalDataAttribute = jVarLocalColsestTr.querySelector('[name="DataAttribute"]');
     let jVarLocalonkeypress = jVarLocalColsestTr.querySelector('[name="onkeypress"]');
     let jVarLocalEnterOnClient = jVarLocalColsestTr.querySelector('[name="EnterOnClient"]');
     let jVarLocalCustomDataList = jVarLocalColsestTr.querySelector('[name="CustomDataList"]');
     let jVarLocalType = jVarLocalColsestTr.querySelector('[name="ClientEval"]');
 
+    let jVarLocalFolderName = jVarLocalCurrentTarget.dataset.foldername;
+    let jVarLocalfilename = jVarLocalCurrentTarget.dataset.filename;
+    let jVarLocalitemname = jVarLocalCurrentTarget.dataset.itemname;
+    let jVarLocalscreenname = jVarLocalCurrentTarget.dataset.screenname;
+
+    let jVarLocalDataAttributeValue = jVarLocalDataAttribute.value;
     let jVarLocalonkeypressValue = jVarLocalonkeypress.checked;
     let jVarLocalEnterOnClientValue = jVarLocalEnterOnClient.checked;
     let jVarLocalCustomDataListValue = jVarLocalCustomDataList.checked;
 
     let jVarLocalTypeValue = jVarLocalType.value;
+
+    // let BodyAsJson = {
+    //     CustomDataList: jVarLocalCustomDataListValue,
+    //     onkeypress: jVarLocalonkeypressValue,
+    //     EnterOnClient: jVarLocalEnterOnClientValue,
+    //     ClientEval: jVarLocalTypeValue
+    // };
 
     let BodyAsJson = {
         CustomDataList: jVarLocalCustomDataListValue,
@@ -18,7 +32,17 @@ let StartFunc = () => {
         EnterOnClient: jVarLocalEnterOnClientValue,
         ClientEval: jVarLocalTypeValue
     }
-    return BodyAsJson;
+
+    let jVarLocalReturnObject = {
+        folderName: jVarLocalFolderName,
+        FileName: jVarLocalfilename,
+        ItemName: jVarLocalitemname,
+        ScreenName: jVarLocalscreenname,
+        DataAttribute: jVarLocalDataAttributeValue,
+        BodyAsJson
+    };
+
+    return jVarLocalReturnObject;
 };
 
 export { StartFunc };
