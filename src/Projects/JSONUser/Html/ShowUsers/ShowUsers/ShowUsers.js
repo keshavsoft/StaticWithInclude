ec4f552484af0e7769b084552cc272623b08e689
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-07-16 22:59:02 +0530

    keshav

--- src/Projects/JSONUser/Html/ShowUsers/ShowUsers/ShowUsers.js
+++ src/Projects/JSONUser/Html/ShowUsers/ShowUsers/ShowUsers.js
@@ -1,32 +1,38 @@
 import { StartFunc as StartFuncShowData } from "./FetchFuncs/ShowData.js";
-import { StartFunc as CheckTokenStartFunc } from "../../AdminData/CheckToken.js";
+// import { StartFunc as CheckTokenStartFunc } from "../../AdminData/CheckToken.js";
 import { StartFunc as AddListenersStartFunc } from "./AddListeners.js";
-
+import { StartFunc as StartFuncAdminData } from "../../AdminData/StartFunc.js";
 
 let localJsonDataFetch = await fetch('/JSONUser/Js/ConfigKeys.json');
 let ResponseJsonData = await localJsonDataFetch.json();
 
-// let jVarGlobalTokenName = "KUMAToken";
-// let jVarGlobalUserKey = "KUMAUserName";
-// let jVarGlobalFirmKey = "FirmDetails";
-// let jVarGlobalModalId = "LoginModalId";
-// let jVarGlobalAdminSubRoute = "JSONUser";
-
 let StartFunc = async () => {
-    AddListenersStartFunc({
-        inSubRoute: ResponseJsonData.SubRoute,
-        inUserKey: ResponseJsonData.UserKey,
-        inFirmKey: ResponseJsonData.FirmKey,
-        inTokenName: ResponseJsonData.TokenName,
-        inModalId: ResponseJsonData.ModalId
-    });
+    let jVarLocalFromAdmin = await StartFuncAdminData();
 
-    CheckTokenStartFunc({
-        inUserKey: ResponseJsonData.UserKey,
-        inFirmKey: ResponseJsonData.FirmKey,
-        inTokenName: ResponseJsonData.TokenName,
-        inModalId: ResponseJsonData.ModalId
-    });
+    if (jVarLocalFromAdmin) {
+        AddListenersStartFunc({
+            inSubRoute: ResponseJsonData.SubRoute,
+            inUserKey: ResponseJsonData.UserKey,
+            inFirmKey: ResponseJsonData.FirmKey,
+            inTokenName: ResponseJsonData.TokenName,
+            inModalId: ResponseJsonData.ModalId
+        });
+    };
+
+    // AddListenersStartFunc({
+    //     inSubRoute: ResponseJsonData.SubRoute,
+    //     inUserKey: ResponseJsonData.UserKey,
+    //     inFirmKey: ResponseJsonData.FirmKey,
+    //     inTokenName: ResponseJsonData.TokenName,
+    //     inModalId: ResponseJsonData.ModalId
+    // });
+
+    // CheckTokenStartFunc({
+    //     inUserKey: ResponseJsonData.UserKey,
+    //     inFirmKey: ResponseJsonData.FirmKey,
+    //     inTokenName: ResponseJsonData.TokenName,
+    //     inModalId: ResponseJsonData.ModalId
+    // });
 
     StartFuncShowData();
 };
