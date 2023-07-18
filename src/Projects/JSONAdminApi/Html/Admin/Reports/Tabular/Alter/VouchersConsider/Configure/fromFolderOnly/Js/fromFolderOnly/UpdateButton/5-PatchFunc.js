f0657602a4968a63c409d22a876491a0b59aa60f
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-18 19:47:03 +0530

    SREENIVAS

--- src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/Configure/fromFolderOnly/Js/fromFolderOnly/UpdateButton/5-PatchFunc.js
+++ src/Projects/JSONAdminApi/Html/Admin/Reports/Tabular/Alter/VouchersConsider/Configure/fromFolderOnly/Js/fromFolderOnly/UpdateButton/5-PatchFunc.js
@@ -1,8 +1,33 @@
 import ApiConfigJson from '../../../../../../../../../ApiConfig.json' assert {type: 'json'};
+import { StartFunc as StartFuncAfterFetchFunc } from "./6-AfterFetchFunc.js";
 
-let StartFunc = async ({ inFetchBody }) => {
+
+let StartFunc = async ({ inFetchBody, inBodyData }) => {
+
+    const newPath = `/${ApiConfigJson.ProjectName}/Html/Admin/Reports/Tabular/Alter/VouchersConsider/Configure/fromFolderOnly/FromFolderOnly.html` // Replace this with the new path you want
+
+    Swal.fire({
+        title: 'Do you want to Update the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Update',
+        denyButtonText: `Don't Update`,
+        focusDeny: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localFetchFunc({ inFetchBody, inBodyData });
+        } else if (result.isDenied) {
+            window.location.href = newPath;
+            Swal.fire('Changes are not saved', '', 'info');
+
+        };
+    });
+
+};
+
+const localFetchFunc = async ({ inFetchBody, inBodyData }) => {
     let jFetchUrl = `/${ApiConfigJson.ProjectName}/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys/FromFolderOnly`;
-    
+
     let jLocalFetchConfig = {
         method: "PATCH",
         headers: {
@@ -10,11 +35,17 @@ let StartFunc = async ({ inFetchBody }) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(inFetchBody)
-    }
+    };
 
     let response = await fetch(jFetchUrl, jLocalFetchConfig);
 
-    return response.status;
-};
+    let jVarLocalResponseData = await response.json();
+    console.log('jVarLocalResponseData:', jVarLocalResponseData, inBodyData);
+
+    StartFuncAfterFetchFunc({ inResponse: jVarLocalResponseData, inBodyData });
+
+    // return await response.status;
+
+}
 
 export { StartFunc };
