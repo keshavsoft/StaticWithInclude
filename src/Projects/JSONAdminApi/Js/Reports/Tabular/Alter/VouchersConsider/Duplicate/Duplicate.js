4b17ad537ad843238787cb05e4ae2299ad689c33
Satish Addanki <satishaddanki321@gmail.com>
2023-07-21 15:45:47 +0530

    satish

--- src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Duplicate/Duplicate.js
+++ src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Duplicate/Duplicate.js
@@ -1,6 +1,8 @@
 import { jFCreateFoldersToDom } from "./ShowOnDom/ShowOnDom.js";
-import { jFCloneFunc as jFCloneFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
+// import { jFCloneFunc as jFCloneFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
+import { StartFunc as StartFuncCloneButton } from "./CloneButton/1-ClickFunc.js";
 
 jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
-    jFCloneFuncAddlisteners();
+    // jFCloneFuncAddlisteners();
+    StartFuncCloneButton();
 });
