d4ccacab4545b2fefb5ad358102f2db29323b58a
Satish Addanki <satishaddanki321@gmail.com>
2023-07-22 14:31:52 +0530

    satish

--- src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Columns/DefaultValue/DefaultValue.js
+++ src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Columns/DefaultValue/DefaultValue.js
@@ -1,6 +1,6 @@
 import { jFCreateFoldersToDom } from "./ShowOnDom/ShowOnDom.js";
-import { jFCloneFunc as jFCloneFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
+import { StartFunc as StartFuncUpdateButton } from "./UpdateButton/1-ClickFunc.js";
 
 jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
-    jFCloneFuncAddlisteners();
+    StartFuncUpdateButton();
 });
