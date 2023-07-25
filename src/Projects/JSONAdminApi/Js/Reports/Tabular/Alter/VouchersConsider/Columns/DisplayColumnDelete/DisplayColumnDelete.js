a36f302a6055e3851243bf02d55acb462afbfb48
Satish Addanki <satishaddanki321@gmail.com>
2023-07-25 19:58:50 +0530

    satish

--- src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Columns/DisplayColumnDelete/DisplayColumnDelete.js
+++ src/Projects/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Columns/DisplayColumnDelete/DisplayColumnDelete.js
@@ -1,6 +1,8 @@
 import { jFCreateFoldersToDom } from "./ShowOnDom/ShowOnDom.js";
-import { jFCloneFunc as jFCloneFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
+import { StartFunc as StartFuncDeleteButton } from "./DeleteButton/1-ClickFunc.js";
+// import { jFCloneFunc as jFCloneFuncAddlisteners } from "./Addlisteners/Addlisteners.js";
 
 jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
-    jFCloneFuncAddlisteners();
+    // jFCloneFuncAddlisteners();
+    StartFuncDeleteButton();
 });
