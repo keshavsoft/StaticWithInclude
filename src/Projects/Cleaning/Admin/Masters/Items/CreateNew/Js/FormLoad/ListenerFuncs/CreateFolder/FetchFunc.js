2dfd89bb668ab3d30296feecfa187f9ab34fc3f4
Satish Addanki <satishaddanki321@gmail.com>
2023-07-27 19:11:30 +0530

    satish

--- src/Projects/Cleaning/Admin/Masters/Items/CreateNew/Js/FormLoad/ListenerFuncs/CreateFolder/FetchFunc.js
+++ src/Projects/Cleaning/Admin/Masters/Items/CreateNew/Js/FormLoad/ListenerFuncs/CreateFolder/FetchFunc.js
@@ -1,4 +1,7 @@
-import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../../../../ToLocalStorage/Items/Single.js";
+// import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../../../../ToLocalStorage/Items/Single.js";
+import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../POS/Booking/ToLocalStorage/Items/Single.js";
+
+
 
 let StartFunc = ({ inBodyData }) => {
     return jFToLocalStorage({ inBodyData });
