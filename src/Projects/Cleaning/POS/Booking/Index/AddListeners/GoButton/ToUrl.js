e223e9c3283fbe46944ec0a3d3ae79ed60611449
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-28 17:57:46 +0530

    SREENIVAS

--- src/Projects/Cleaning/POS/Booking/Index/AddListeners/GoButton/ToUrl.js
+++ src/Projects/Cleaning/POS/Booking/Index/AddListeners/GoButton/ToUrl.js
@@ -1,7 +1,7 @@
 import ConfigJson from "../../../../../Config.json" assert {type: 'json'};
 
 let StartFunc = ({ inCustomerName, inCustomerMobile}) => {
-    let jVarLocalToUrl = "/public/Html/Cleaning/POS/Booking/BranchWise/NewOrder/NewOrder.html";
+    let jVarLocalToUrl = "/Cleaning/POS/Booking/BranchWise/NewOrder/NewOrder.html";
     let jVarLocalBranchName = ConfigJson.BranchName;
     let jVarLocalAddNew = "true";
 
