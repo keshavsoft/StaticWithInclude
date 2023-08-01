3ca57f39ea829d2a143d4b267e1164d425b1e855
Satish Addanki <satishaddanki321@gmail.com>
2023-08-01 16:34:13 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchWise/OrderWithoutCheckout/FormLoad/FromUrlParams/OldOrder/StartFunc.js
+++ src/Projects/Cleaning/POS/Booking/BranchWise/OrderWithoutCheckout/FormLoad/FromUrlParams/OldOrder/StartFunc.js
@@ -1,13 +1,10 @@
 import { StartFunc as StartFuncFromFetch } from "../../../FetchFuncs/FromServer/OldOrder/2-ButtonClickFunc.js";
 import { StartFunc as StartFuncAddToDom } from "../../AddToDom.js";
 import { StartFunc as StartFuncShowOnDom } from "../../../ShowOnDom/StartFunc.js";
-// import { StartFunc as StartFuncFromLocalStorage } from "../../../../../../../../../../FromLocalStorage/OrdersData/FromPk.js";
-
 import { StartFunc as StartFuncFromLocalStorage } from "../../../../../FromLocalStorage/OrdersData/FromPk.js";
-
-// import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../../../ToLocalStorage/ItemsInOrder/Bulk.js";
-
 import { StartFunc as StartFuncToLocalStorage } from "../../../../../ToLocalStorage/ItemsInOrder/Bulk.js";
+import { StartFunc as StartFuncToLocalStorageAddOn } from "../../../../../ToLocalStorage/AddOnData/Bulk.js";
+
 
 let StartFunc = () => {
     let jVarLocalRowPk = getUrlQueryParams({ inGetKey: "OrderNumber" });
@@ -15,6 +12,7 @@ let StartFunc = () => {
     StartFuncFromFetch({ inPk: jVarLocalRowPk });
 
     jFLocalFromOrdersDataToItemsInOrder();
+    jFLocalAddAddOnData();
 
     StartFuncAddToDom();
     StartFuncShowOnDom({ inPk: jVarLocalRowPk });
@@ -29,6 +27,17 @@ let jFLocalFromOrdersDataToItemsInOrder = () => {
     };
 
     StartFuncToLocalStorage({ inData: jVarLocalFromLocalStorage.JsonData.ItemsInOrder });
+};
+
+let jFLocalAddAddOnData = () => {
+    let jFLocalOrderNumber = getUrlQueryParams({ inGetKey: "OrderNumber" });
+    let jVarLocalFromLocalStorage = StartFuncFromLocalStorage({ inPk: jFLocalOrderNumber });
+
+    if (jVarLocalFromLocalStorage.KTF === false) {
+        return false;
+    };
+
+    StartFuncToLocalStorageAddOn({ inData: jVarLocalFromLocalStorage.JsonData.AddOnData });
 };
 
 let getUrlQueryParams = ({ inGetKey }) => {
