bcc8fc0849b030bc244501d5f8b2da7e1666373a
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-07 20:39:03 +0530

    keshav

--- src/Projects/Cleaning/POS/Booking/BranchWise/Gurudwara/AddListeners/AddItemToServer/4-PreparePostData/EntryFile.js
+++ src/Projects/Cleaning/POS/Booking/BranchWise/Gurudwara/AddListeners/AddItemToServer/4-PreparePostData/EntryFile.js
@@ -1,25 +1,20 @@
 import { StartFunc as StartFuncPrepareItemsInOrder } from "./PrepareItemsInOrder.js";
 import { StartFunc as StartFuncCustomerData } from "./CustomerData.js";
 import { StartFunc as StartFuncOrderData } from "./OrderData.js";
+import ConfigJson from "./Config.json" assert {type: 'json'};
 
 const StartFunc = () => {
     let jVarLocalToLocalStorage = {};
 
     jVarLocalToLocalStorage.inJsonConfig = {};
     jVarLocalToLocalStorage.inJsonConfig.inFolderName = "Transactions";
-    jVarLocalToLocalStorage.inJsonConfig.inJsonFileName = "Gurudwara.json";
+    jVarLocalToLocalStorage.inJsonConfig.inJsonFileName = ConfigJson.JsonFileName;
 
     jVarLocalToLocalStorage.inItemConfig = {};
     jVarLocalToLocalStorage.inItemConfig.inItemName = 'Orders';
 
     let jVarLocalCustomerData = StartFuncCustomerData();
     let jVarLocalOrderData = StartFuncOrderData();
-
-    // jVarLocalToLocalStorage.inPostData = {
-    //     ItemsInOrder: {},
-    //     CustomerData: { ...jVarLocalCustomerData },
-    //     OrderData: { ...jVarLocalOrderData }
-    // };
 
     jVarLocalToLocalStorage.inPostData = {};
     jVarLocalToLocalStorage.inPostData.ItemsInOrder = {};
