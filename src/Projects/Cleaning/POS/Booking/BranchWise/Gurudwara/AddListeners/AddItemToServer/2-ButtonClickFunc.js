f1d2b311876504774f0cece5b1955875444b0b9a
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-07 20:38:00 +0530

    keshav

--- src/Projects/Cleaning/POS/Booking/BranchWise/Gurudwara/AddListeners/AddItemToServer/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/BranchWise/Gurudwara/AddListeners/AddItemToServer/2-ButtonClickFunc.js
@@ -2,6 +2,7 @@ import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
 import { StartFunc as StartFuncPreparePostData } from "./4-PreparePostData/EntryFile.js";
 import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
 import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
+import ApiConfigJson from "../../../../../../" assert {type: 'json'};
 
 let StartFunc = async() => {
     let jVarLocalCurrentTarget = event.currentTarget;
