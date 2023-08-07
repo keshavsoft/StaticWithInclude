d74428cf23b5b4a1ac6eac9f11ef10c6cad39b35
Satish Addanki <satishaddanki321@gmail.com>
2023-08-07 20:50:24 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchWise/Gurudwara/AddListeners/AddItemToServer/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/BranchWise/Gurudwara/AddListeners/AddItemToServer/2-ButtonClickFunc.js
@@ -2,7 +2,6 @@ import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
 import { StartFunc as StartFuncPreparePostData } from "./4-PreparePostData/EntryFile.js";
 import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
 import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
-import ApiConfigJson from "../../../../../../" assert {type: 'json'};
 
 let StartFunc = async() => {
     let jVarLocalCurrentTarget = event.currentTarget;
