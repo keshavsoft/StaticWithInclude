17e313ac81e8f6ce41b522ababcab9cef1e0e103
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-08-10 18:45:32 +0530

    SREENIVAS

--- src/Projects/Garments/Purchases/Vouchers/ShowAll/AddListeners.js
+++ src/Projects/Garments/Purchases/Vouchers/ShowAll/AddListeners.js
@@ -38,7 +38,29 @@ let LocalPostQrCodeButtonFuncs = ({ inProjectName }) => {
             };
 
             if (jVarLocalFromGet.KTF) {
-                window.location.href = `../Show/Show.html?RowPK=${jVarLocalRowPk}`;
+                // window.location.href = `../ShowAll/ShowAll.html?RowPK=${jVarLocalRowPk}`;
+                let timerInterval
+                Swal.fire({
+                    title: 'QR Code Generatimg..!',
+                    html: 'Generating QR <b></b>',
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: () => {
+                        Swal.showLoading()
+                        const b = Swal.getHtmlContainer().querySelector('b')
+                        timerInterval = setInterval(() => {
+                            b.textContent = Swal.getTimerLeft()
+                        }, 100)
+                    },
+                    willClose: () => {
+                        clearInterval(timerInterval)
+                    }
+                }).then((result) => {
+                    /* Read more about handling dismissals below */
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        console.log('I was closed by the timer')
+                    }
+                })
             };
         });
     };
