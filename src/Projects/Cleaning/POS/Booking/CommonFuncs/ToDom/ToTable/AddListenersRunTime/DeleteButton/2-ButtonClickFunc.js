1a92b48d8697ce6375997dbb519b55272ed4172a
Satish Addanki <satishaddanki321@gmail.com>
2023-08-12 16:23:40 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/CommonFuncs/ToDom/ToTable/AddListenersRunTime/DeleteButton/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/CommonFuncs/ToDom/ToTable/AddListenersRunTime/DeleteButton/2-ButtonClickFunc.js
@@ -1,6 +1,6 @@
 import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
 import { StartFunc as StartFuncPreparePostData } from "./4-PreparePostData.js";
-// import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
+import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
 // import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
 
 let StartFunc = (event) => {
@@ -24,16 +24,21 @@ const jFLocalSwal = ({ inItemSerial }) => {
         allowOutsideClick: () => !Swal.isLoading()
     }).then((result) => {
         if (result.isConfirmed) {
-            let jVarLocalPreparePostData = StartFuncPreparePostData({ inItemSerial });
-            let response = StartFuncFetchFunc({ inBodyData: jVarLocalPreparePostData }).then(PromiseData => {
-                console.log("PromiseData", PromiseData);
-                if (PromiseData.KTF === true) {
-                    window.location.href = "";
-                }
-            });
-            console.log("jVarLocalFromChange : ", response);
+            jFLocalSwalAfterSwal({ inItemSerial });
         }
     });
 };
+
+let jFLocalSwalAfterSwal = ({ inItemSerial }) => {
+    if (StartFuncCheckBeforeFetch({ inItemSerial })) {
+        let jVarLocalPreparePostData = StartFuncPreparePostData({ inItemSerial });
+
+        StartFuncFetchFunc({ inBodyData: jVarLocalPreparePostData }).then(PromiseData => {
+            if (PromiseData.KTF === true) {
+                window.location.href = "";
+            }
+        });
+    }
+}
 
 export { StartFunc };
