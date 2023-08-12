1e087be25ba8095b2cf482f7e6daf7f4fd29b619
Satish Addanki <satishaddanki321@gmail.com>
2023-08-12 16:50:46 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/Gurudwara/NewOrder/ToStaticDom/AddListenerFuncs/StartFunc.js
+++ src/Projects/Cleaning/POS/Booking/Gurudwara/NewOrder/ToStaticDom/AddListenerFuncs/StartFunc.js
@@ -1,9 +1,7 @@
 import { StartFunc as StartFuncCategoryRadio } from "./CategoryRadio/StartFunc.js";
 import { StartFunc as StartFuncItemSelect } from "./ItemSelect/OnChange/StartFunc.js";
-import { StartFunc as StartFuncCheckOutButton } from "../../AddListeners/OrderInfo/CheckOutButton/ClickEvent.js";
 
 let StartFunc = () => {
-    StartFuncCheckOutButton();
     StartFuncItemSelect();
     StartFuncCategoryRadio();
 };
