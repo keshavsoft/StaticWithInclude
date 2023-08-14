5c4c0febc9f937d5b31f1f895455d72def71ccf3
Satish Addanki <satishaddanki321@gmail.com>
2023-08-14 19:26:22 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/Gurudwara/OrderWithoutCheckout/FetchFuncs/FromServer/OldOrder/4-PreparePostData.js
+++ src/Projects/Cleaning/POS/Booking/Gurudwara/OrderWithoutCheckout/FetchFuncs/FromServer/OldOrder/4-PreparePostData.js
@@ -1,8 +1,10 @@
+import ApiConfigJson from "../../../../ApiConfig.json" assert {type: 'json'};
+
 let StartFunc = ({ inPk }) => {
     let jVarLocalBody = {
-        FolderName: "Transactions",
-        FileNameOnly: "Gurudwara",
-        ItemName: "Orders",
+        FolderName: ApiConfigJson.ForFetch.FolderName,
+        FileNameOnly: ApiConfigJson.ForFetch.FileNameOnly,
+        ItemName: ApiConfigJson.ForFetch.ItemName,
         JsonPk: inPk
     };
 
--- src/Projects/Cleaning/POS/Booking/Gurudwara/OrderWithoutCheckout/FormLoad/ToDom/ToTable/ToOrderItems.js
+++ src/Projects/Cleaning/POS/Booking/Gurudwara/OrderWithoutCheckout/FormLoad/ToDom/ToTable/ToOrderItems.js
@@ -1,63 +1,108 @@
+import { StartFunc as StartFuncToRow } from "./ToRow/ToOrderItems.js";
+
+import { StartFunc as StartFuncShowTotals } from "./ToFooter/ShowTotals.js";
+import { StartFunc as StartFuncToFactories } from "./ToRow/ToFactories.js";
+import { StartFunc as StartFuncToDeliveryDateTime } from "./ToRow/ToDeliveryDateTime.js";
+
+// import { StartFunc as StartFuncOrderItemsToShow } from "../../../FromLocalStorage/OrderItemsToShow/Bulk.js";
+// import { StartFunc as StartFuncPrepareForOrderItemsTable } from "../../../ToLocalStorage/OrderItemsToShow/PrepareForOrderItemsTable.js";
+import { StartFunc as StartFuncPrepareForOrderItemsTable } from "../../../../../ToLocalStorage/OrderItemsToShow/PrepareForOrderItemsTable.js";
+import { StartFunc as StartFuncOrderItemsToShow } from "../../../../../FromLocalStorage/OrderItemsToShow/Bulk.js";
+
+
+
+let jVarCommonTableBodyId = "ItemsTableBodyId";
+
 let StartFunc = () => {
-    let jVarLocalItemsTableBodyId = "ItemsTableBodyId";
+    jFClearDom();
+    StartFuncPrepareForOrderItemsTable();
 
-    let jVarLocalItemsInOrder = localStorage.getItem("ItemsInOrder");
-    let jVarLocaljVarLocalItemsInOrderJson = JSON.parse(jVarLocalItemsInOrder);
-
-    jVarLocaljVarLocalItemsInOrderJson.forEach(element => {
-        jFLocalItemsInsertRow({
-            inTableBodyId: jVarLocalItemsTableBodyId,
-            inCategory: element.Category,
-            inItemName: element.ItemName,
-            inWashType: element.WashType,
-            inPcs: element.Pcs,
-            inItemRate: element.Rate,
-            inAddOn: element.AddOn,
-            inTotal: element.Total
-        });
-    });
+    let jVarLocalJsonData = StartFuncOrderItemsToShow();
+    
+    if (jVarLocalJsonData === null === false) {
+        if ("BodyData" in jVarLocalJsonData) {
+            ShowOnDom({ inJsonData: jVarLocalJsonData.BodyData });
+        };
+    };
 };
 
-let jFLocalItemsInsertRow = ({ inTableBodyId, inCategory, inItemName, inWashType, inPcs, inItemRate, inAddOn, inTotal }) => {
-    var table = document.getElementById(inTableBodyId);
-    let jVarLocalTableRowLength = table.rows.length
+let jFClearDom = () => {
+    let jVarLocalItemsTableBodyId = jVarCommonTableBodyId;
+    var jVarLocalHtmlTableBody = document.getElementById(jVarLocalItemsTableBodyId);
 
-    // Create an empty <tr> element and add it to the 1st position of the table:
-    var row = table.insertRow(0);
-
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
-    var cell7 = row.insertCell(6);
-    var cell8 = row.insertCell(7);
-
-    cell5.className = "text-end";
-    cell6.className = "text-end";
-    cell7.className = "text-end";
-    cell8.className = "text-end";
-
-    let jVarLocalSerialButton = document.createElement("input");
-    jVarLocalSerialButton.type = "button";
-    jVarLocalSerialButton.value = jVarLocalTableRowLength + 1;
-
-    // class="text-end"
-
-    // Add some text to the new cells:
-    //cell1.innerHTML = jVarLocalTableRowLength + 1;
-
-    cell1.appendChild(jVarLocalSerialButton);
-
-    cell2.innerHTML = inCategory;
-    cell3.innerHTML = inItemName;
-    cell4.innerHTML = inWashType;
-    cell5.innerHTML = inPcs;
-    cell6.innerHTML = `₹ ${inItemRate}`;
-    cell7.innerHTML = `₹ ${inAddOn}`;
-    cell8.innerHTML = `₹ ${inTotal}`;
+    jVarLocalHtmlTableBody.innerHTML = "";
 };
+
+let ShowOnDom = ({ inJsonData }) => {
+    let jVarLocalItemsTableBodyId = jVarCommonTableBodyId;
+    var jVarLocalHtmlTableBody = document.getElementById(jVarLocalItemsTableBodyId);
+
+    let jVarLocaljVarLocalItemsInOrderJson = inJsonData;
+
+    Object.entries(jVarLocaljVarLocalItemsInOrderJson).forEach(
+        ([key, element]) => {
+            jFLocalItemsInsertRowFromTemplate({
+                inRowPk: parseInt(key),
+                inTableBodyId: jVarLocalHtmlTableBody,
+                inCategory: element.Category,
+                inItemName: element.ItemName,
+                inItemNamePk: element.ItemNamePk,
+                inWashType: element.WashType,
+                inPcs: element.Pcs,
+                inDisPer: element.DisPer,
+                inItemRate: element.Rate,
+                inAddOn: element.AddOn,
+                inTotal: element.Total,
+                inlocation: element.location,
+                inlocationPk: element.locationPk,
+                inDeliveryDateTime: element.DeliveryDateTime
+            });
+        }
+    );
+
+    StartFuncShowTotals({ inJsonData: Object.values(jVarLocaljVarLocalItemsInOrderJson) });
+
+    let jVarLocalItemsShowCollapseId = document.getElementById("ItemsShowCollapseId");
+
+    let jVarLocalBSItemsShowCollapseId = bootstrap.Collapse.getOrCreateInstance(jVarLocalItemsShowCollapseId);
+
+    jVarLocalBSItemsShowCollapseId.show();
+
+};
+
+let jFLocalItemsInsertRowFromTemplate = ({ inRowPk, inTableBodyId, inCategory, inItemName, inItemNamePk, inWashType, inWashTypePk, inPcs, inItemRate, inDisPer, inAddOn, inTotal, inlocation, inlocationPk, inDeliveryDateTime }) => {
+    var table = inTableBodyId;
+    let jVarLocalTemplateForOrderItemsTableRow = document.getElementById("TemplateForOrderItemsTableRow");
+
+    const clone = jVarLocalTemplateForOrderItemsTableRow.content.cloneNode(true);
+
+    let jVarLocalOrderItemsCategoryClass = clone.querySelector(".OrderItemsCategoryClass");
+    jVarLocalOrderItemsCategoryClass.innerHTML = inCategory;
+
+    let jVarLocalOrderItemsOrderItemsItemNameClass = clone.querySelector(".OrderItemsItemNameClass");
+    jVarLocalOrderItemsOrderItemsItemNameClass.dataset.pk = inItemNamePk;
+    jVarLocalOrderItemsOrderItemsItemNameClass.innerHTML = inItemName;
+
+    let jVarLocalOrderItemsOrderItemsRateClass = clone.querySelector(".OrderItemsRateClass");
+    jVarLocalOrderItemsOrderItemsRateClass.innerHTML = inItemRate;
+
+    let jVarLocalOrderItemsOrderDisPerClass = clone.querySelector(".OrderItemsDisPerClass");
+    jVarLocalOrderItemsOrderDisPerClass.innerHTML = inDisPer;
+
+    let jVarLocalOrderItemsOrderItemsItemWashClass = clone.querySelector(".OrderItemsItemWashClass");
+    jVarLocalOrderItemsOrderItemsItemWashClass.dataset.pk = inWashTypePk;
+    jVarLocalOrderItemsOrderItemsItemWashClass.innerHTML = inWashType;
+
+    let jVarLocalOrderItemsOrderItemsTotalClass = clone.querySelector(".OrderItemsTotalClass");
+    jVarLocalOrderItemsOrderItemsTotalClass.innerHTML = inTotal;
+
+    StartFuncToFactories({ inlocation, inlocationPk, inClonedTemplateRow: clone });
+    StartFuncToDeliveryDateTime({ inDeliveryDateTime, inClonedTemplateRow: clone });
+
+    StartFuncToRow({ inItemSerial: inRowPk, inClonedTemplateRow: clone, inPcs });
+
+    table.appendChild(clone);
+};
+
 
 export { StartFunc };
