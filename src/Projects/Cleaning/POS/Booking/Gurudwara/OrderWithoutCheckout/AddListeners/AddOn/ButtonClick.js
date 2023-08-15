c5a1065fac5c581192a7b4ddec60113a923d8701
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-15 11:34:14 +0530

    keshav

--- src/Projects/Cleaning/POS/Booking/Gurudwara/OrderWithoutCheckout/AddListeners/AddOn/ButtonClick.js
+++ src/Projects/Cleaning/POS/Booking/Gurudwara/OrderWithoutCheckout/AddListeners/AddOn/ButtonClick.js
@@ -1,38 +1,9 @@
-import { StartFunc as StartFuncPrepareData } from "./PrepareData.js";
-import { StartFunc as StartFuncCheckBeforeSave } from "./CheckBeforeSave.js";
-import { StartFunc as StartFuncToLocalStorage } from "../../ToLocalStorage/ToAddOnData.js";
-
-// import { StartFunc as StartFuncToAddOns } from "../../ToDom/ToTable/AddOnSection/StartFunc.js";
-
-//import { StartFunc as StartFuncToDomToOrderItems } from "../../ToDom/ToTable/ToOrderItems.js";
-
-import { StartFunc as StartFuncToDomToOrderItems } from "../../ToDom/ToTable/ToOrderItems.js";
-
 import { StartFunc as StartFuncAddOnInsertButtonClick } from "./AddOnInsertButton/2-ButtonClickFunc.js";
 
 let StartFunc = () => {
     let jVarLocalAddOnInsertButtonId = document.getElementById("AddOnInsertButtonId");
 
     jVarLocalAddOnInsertButtonId.addEventListener("click", StartFuncAddOnInsertButtonClick);
-};
-
-let jFLocalButtonClick = (event) => {
-    event.preventDefault();
-
-    let jVarLocalFromCheck = StartFuncCheckBeforeSave({ inEvent: event });
-
-    if (jVarLocalFromCheck) {
-        let jVarLocalDataToInsert = StartFuncPrepareData();
-
-        let jVarLocalNewPk = StartFuncToLocalStorage(jVarLocalDataToInsert);
-
-        if (jVarLocalNewPk > 0) {
-            StartFuncToDomToOrderItems();
-
-            // StartFuncToAddOns({ inItemSerial: jVarLocalDataToInsert.inAddOnItemSerial });
-
-        };
-    };
 };
 
 export { StartFunc };
