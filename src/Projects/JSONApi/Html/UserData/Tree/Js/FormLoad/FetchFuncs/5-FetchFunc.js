cf68769eeaa7653356b10bc815675f0cab772568
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-15 17:49:37 +0530

    keshav

--- src/Projects/JSONApi/Html/UserData/Tree/Js/FormLoad/FetchFuncs/5-FetchFunc.js
+++ src/Projects/JSONApi/Html/UserData/Tree/Js/FormLoad/FetchFuncs/5-FetchFunc.js
@@ -1,15 +1,10 @@
 import ConfigJson from "../../../../../../Config.json" assert {type: 'json'};
 
-
 let StartFunc = async () => {
-
     let jVarLocalFetchUrl = `/${ConfigJson.Project}/Api/Data/FromFolder/TreeWitData`;
-    
-    let response = await fetch(jVarLocalFetchUrl);
-    let jVarLocalResponse = await response.json();
-    console.log("jVarLocalResponse:",jVarLocalResponse);
 
-    return jVarLocalResponse;
+    let response = await fetch(jVarLocalFetchUrl);
+    jVarLocalPresentViewData = await response.json();
 };
 
 export { StartFunc };
--- src/Projects/JSONApi/Html/UserData/Tree/Js/FormLoad/FetchFuncs/EnteryFile.js
+++ src/Projects/JSONApi/Html/UserData/Tree/Js/FormLoad/FetchFuncs/EnteryFile.js
@@ -2,9 +2,9 @@ import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
 import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
 
 let StartFunc = async () => {
+    await StartFuncFetchFunc();
 
-    let response = await StartFuncFetchFunc();
-    StartFuncAfterFetch({ inFromFetch: response });
+    StartFuncAfterFetch();
 };
 
 export { StartFunc };
