c5a6e7170b3899c4e7e783f919401704d137e8b3
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-08-15 16:48:00 +0530

    SREENIVAS

--- src/Projects/JSONApi/Html/UserData/Tree/Js/FoamLoad/FetchFuncs/6-AfterFetch.js
+++ src/Projects/JSONApi/Html/UserData/Tree/Js/FoamLoad/FetchFuncs/6-AfterFetch.js
@@ -1,5 +1,6 @@
 let StartFunc = ({ inFromFetch }) => {
-    // let data = inFromFetch;
+    let data = inFromFetch;
+    jVarLocalPresentViewData = data;
     // if (data) {
     //     let jVarLocalRawTemplate = document.getElementById("HbsTemplateForFoldersOnly").innerHTML;
     //     document.getElementById("KCont1").innerHTML = Handlebars.compile(jVarLocalRawTemplate)(data.JsonData);
