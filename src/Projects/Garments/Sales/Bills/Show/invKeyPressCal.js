c9c12369cffe18b1d99ab47d1fd2636da70e101e
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-08-16 18:20:41 +0530

    SREENIVAS

--- src/Projects/Garments/Sales/Bills/Show/invKeyPressCal.js
+++ src/Projects/Garments/Sales/Bills/Show/invKeyPressCal.js
@@ -1,56 +1,14 @@
+import { StartFunc as StartFuncDisPersantage } from "./invKeyPressCal/DisPersantage.js";
+import { StartFunc as StartFuncDisRate } from "./invKeyPressCal/DisRate.js";
+import { StartFunc as StartFuncGrossAmout } from "./invKeyPressCal/GrossAmout.js";
+import { StartFunc as StartFuncQty } from "./invKeyPressCal/Qty.js";
+
 let StartFunc = () => {
-    let jVarLocalDisPersantage = document.getElementById("DisPersantage");
-    let jVarLocalRate = document.getElementById("RateId1");
-    let jVarLocalDisRate = document.getElementById("DisRate");
-    let jVarLocalGrossAmout = document.getElementById("GrossAmout");
-
-    jVarLocalDisPersantage.addEventListener("keypress", async (event) => {
-        if (event.keyCode === 13) { // key code of the keybord key
-            event.preventDefault();
-            let localdisPercentage = parseInt(jVarLocalDisPersantage.value);
-            let jVarLocalRatevalue = parseInt(jVarLocalRate.value);
-
-            let jVarLocalCurrentTarget = parseInt((localdisPercentage * jVarLocalRatevalue) / 100);
-            let localWithDisValue = (jVarLocalRatevalue - jVarLocalCurrentTarget)
-            jVarLocalDisRate.value = jVarLocalCurrentTarget;
-            jVarLocalGrossAmout.value = localWithDisValue;
-
-        }
-    });
-
-    jVarLocalDisRate.addEventListener("keypress", async (event) => {
-        if (event.keyCode === 13) { // key code of the keybord key
-            event.preventDefault();
-            let localdisRateValue = parseInt(jVarLocalDisRate.value);
-            let jVarLocalRatevalue = parseInt(jVarLocalRate.value);
-
-            let localWithDisValue = (localdisRateValue / jVarLocalRatevalue) * 100;
-            // let jVarLocalCurrentTarget = parseInt((localWithDisValue / jVarLocalRatevalue) * 100);
-            jVarLocalDisPersantage.value = localWithDisValue;
-            jVarLocalGrossAmout.value = (jVarLocalRatevalue - localdisRateValue);
-
-        }
-    });
-
-    LocalGrossAmoutAddListenerFunc();
+    StartFuncDisPersantage();
+    StartFuncDisRate();
+    StartFuncGrossAmout();
+    StartFuncQty();
 };
 
-let LocalGrossAmoutAddListenerFunc = () => {
-    let jVarLocalDisPersantage = document.getElementById("DisPersantage");
-    let jVarLocalRate = document.getElementById("RateId1");
-    let jVarLocalDisRate = document.getElementById("DisRate");
-    let jVarLocalGrossAmout = document.getElementById("GrossAmout");
-
-    jVarLocalGrossAmout.addEventListener("keypress", async (event) => {
-        if (event.keyCode === 13) { // key code of the keybord key
-            event.preventDefault();
-            let jVarLocalRatevalue = parseInt(jVarLocalRate.value);
-
-            jVarLocalDisRate.value = jVarLocalRatevalue - jVarLocalGrossAmout.value;
-
-            jVarLocalDisPersantage.value = (jVarLocalDisRate.value / jVarLocalRate.value) * 100;
-        }
-    });
-};
 
 export { StartFunc };
