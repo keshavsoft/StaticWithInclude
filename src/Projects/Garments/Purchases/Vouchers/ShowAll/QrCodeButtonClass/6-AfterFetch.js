f7190529f93d88ca7376cc6c5c1b30e5863f00bd
Satish Addanki <satishaddanki321@gmail.com>
2023-08-16 20:42:31 +0530

    satish

--- src/Projects/Garments/Purchases/Vouchers/ShowAll/QrCodeButtonClass/6-AfterFetch.js
+++ src/Projects/Garments/Purchases/Vouchers/ShowAll/QrCodeButtonClass/6-AfterFetch.js
@@ -1,39 +1,50 @@
 let StartFunc = async ({ inFromFetch }) => {
     let LocalReturnObject = { KTF: false, KResult: "", JsonData: {} };
-console.log("inFromFetch:",inFromFetch);
+    console.log("inFromFetch:", inFromFetch);
     if (inFromFetch.KTF === false) {
         // LocalReturnObject.KReason = data.KReason;
         // return await LocalReturnObject;
         Swal.fire({
-            icon: 'success',
+            icon: 'warning',
             title: 'Raised',
             text: inFromFetch.KReason
         });
     } else {
         // LocalReturnObject.JsonData = data.JsonData;
         // LocalReturnObject.KTF = true;
-        let timerInterval
-        Swal.fire({
-            title: 'QR Code Generatimg..!',
-            html: 'Generating QR <b></b>',
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: () => {
-                Swal.showLoading()
-                const b = Swal.getHtmlContainer().querySelector('b')
-                timerInterval = setInterval(() => {
-                    b.textContent = Swal.getTimerLeft()
-                }, 100)
-            },
-            willClose: () => {
-                clearInterval(timerInterval)
-            }
-        }).then((result) => {
-            /* Read more about handling dismissals below */
-            if (result.dismiss === Swal.DismissReason.timer) {
-                console.log('I was closed by the timer')
-            }
-        })
+        let raised = inFromFetch.QrCodesRaised;
+        console.log("inFromFetch.QrCodesRaised", raised);
+        if (raised === 0) {
+            // LocalReturnObject.KReason = data.KReason;
+            // return await LocalReturnObject;
+            Swal.fire({
+                icon: 'error',
+                title: 'Not Raised',
+                text: 'Product Details are Empty'
+            });
+        } else {
+            Swal.fire({
+                    icon: 'success',
+                    title: 'Raised',
+                    text: inFromFetch.QrCodesRaised + ' QrCodes raised successfully'
+                });
+                // title: 'QR Code Generatimg..!',
+                // html: 'Generating QR <b></b>',
+                // timer: 2000,
+                // timerProgressBar: true,
+                // didOpen: () => {
+                //     Swal.showLoading()
+                //     const b = Swal.getHtmlContainer().querySelector('b')
+                //     timerInterval = setInterval(() => {
+                //         b.textContent = Swal.getTimerLeft()
+                //     }, 100)
+                // },
+                // willClose: () => {
+                //     clearInterval(timerInterval)
+                // }
+            
+            
+        };
     };
 
 };
