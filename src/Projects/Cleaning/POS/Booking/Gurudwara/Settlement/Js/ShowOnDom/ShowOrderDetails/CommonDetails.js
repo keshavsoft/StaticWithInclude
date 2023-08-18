0a29e90f9799f3681bd928de14839e6e2b838a5d
keshavsoft <nknnkumar@yahoo.com>
2023-08-18 15:00:17 +0530

    keshav

--- src/Projects/Cleaning/POS/Booking/Gurudwara/Settlement/Js/ShowOnDom/ShowOrderDetails/CommonDetails.js
+++ src/Projects/Cleaning/POS/Booking/Gurudwara/Settlement/Js/ShowOnDom/ShowOrderDetails/CommonDetails.js
@@ -1,24 +1,8 @@
-// import { StartFunc as StartFuncFromLocalStorage } from "../../../../../FromLocalStorage/OrdersData/FromPk.js";
-// import { StartFunc as StartFuncPrepareForOrderItemsTable } from "../../../../../ToLocalStorage/OrderItemsToShow/PrepareForOrderItemsTable.js";
+const StartFunc = ({ inData }) => {
+    let jVarLocalData = inData;
 
-import { StartFunc as StartFuncFromLocalStorageOrderItemsToShow } from "../../../../../FromLocalStorage/OrderItemsToShow/Bulk.js";
-
-import { StartFunc as StartFuncFromLocalStorage } from "../../../../../FromLocalStorage/OrdersData/FromPkToLocalStorage.js";
-
-import ApiConfigJson from "../../../../ApiConfig.json" assert { type: "json" };
-
-const StartFunc = ({ inPk }) => {
-    let jVarLocalData = StartFuncFromLocalStorage({
-        inPk,
-        inBranchName: ApiConfigJson.BranchName
-    });
-
-    let jVarLocalDataNeeded = StartFuncFromLocalStorageOrderItemsToShow();
-    
     jFLocalCustomerName({ inOrderInfoCustomerNameId: jVarLocalData.CustomerData.CustomerName });
     jFLocalOrderInfoCustomerMobileId({ inOrderInfoCustomerMobileId: jVarLocalData.CustomerData.CustomerMobile });
-
-    jFLocalOrderAmount({ inData: jVarLocalDataNeeded.BodyData });
 
     jFLocalBranchNameId({ inOrderInfoCustomerBranchId: jVarLocalData.OrderData.BranchName });
     jFLocalToInputOrderDate({ inOrderDate: jVarLocalData.OrderData.Currentdateandtime });
@@ -29,16 +13,6 @@ let jFLocalOrderNumberId = ({ inOrderNumberId }) => {
     let jVarLocalHtmlId = 'OrderNumberId';
     let jVarLocalOrderNumberId = document.getElementById(jVarLocalHtmlId);
     jVarLocalOrderNumberId.innerHTML = inOrderNumberId;
-};
-
-let jFLocalOrderAmount = ({ inData }) => {
-    let jVarLocalItemsArray = Object.values(inData).map(element => {
-        return element.Total;
-    });
-
-    const sum = jVarLocalItemsArray.reduce((partialSum, a) => partialSum + a, 0);
-
-    jFLocalOrderAmountId({ inOrderAmountId: sum });
 };
 
 let jFLocalCustomerName = ({ inOrderInfoCustomerNameId }) => {
@@ -58,12 +32,6 @@ let jFLocalToInputOrderDate = ({ inOrderDate }) => {
     let jVarLocalHtmlId = 'OrderDate';
     let jVarLocalOrderDate = document.getElementById(jVarLocalHtmlId);
     jVarLocalOrderDate.value = inOrderDate;
-};
-
-let jFLocalOrderAmountId = ({ inOrderAmountId }) => {
-    let jVarLocalHtmlId = 'OrderAmountId';
-    let jVarLocalOrderAmountId = document.getElementById(jVarLocalHtmlId);
-    jVarLocalOrderAmountId.innerHTML = inOrderAmountId;
 };
 
 let jFLocalOrderInfoCustomerMobileId = ({ inOrderInfoCustomerMobileId }) => {
--- src/Projects/Cleaning/POS/Booking/Gurudwara/Settlement/Js/ShowOnDom/StartFunc.js
+++ src/Projects/Cleaning/POS/Booking/Gurudwara/Settlement/Js/ShowOnDom/StartFunc.js
@@ -1,5 +1,7 @@
-import { StartFunc as StartFuncShowOrderDetails } from "./ShowOrderDetails/StartFunc.js";
+// import { StartFunc as StartFuncShowOrderDetails } from "./ShowOrderDetails/StartFunc.js";
 import { StartFunc as StartFuncShowSettlementDetails } from "./ShowSettlementDetails/StartFunc.js";
+
+import { StartFunc as StartFuncShowOrderDetails } from "./ShowOrderDetails/EntryFile.js";
 
 const StartFunc = ({ inPk }) => {
     StartFuncShowOrderDetails({ inPk });
