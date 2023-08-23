4cebbb1e70ef03b79a9da5b8d4339b3baa46e5c0
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-08-23 18:37:39 +0530

    SREENIVAS

---
+++ src/Projects/JSONAdminApi/Js/Columns/Tabular/Duplicate/DataDuplicate/DuplicateFiles/DuplicateWithButton/4-PreparePostData.js
@@ -0,0 +1,19 @@
+let StartFunc = ({ CurrentTarget }) => {
+    let jVarLocalCurrentTarget = CurrentTarget;
+
+    let jVarLocalFolderName = jVarLocalCurrentTarget.dataset.foldername;
+    let jVarLocalfilename = jVarLocalCurrentTarget.dataset.filename;
+
+    let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
+    let jVarLocalCloneName = jVarLocalColsestTr.querySelector('[name="CloneName"]');
+
+    let jVarLocalCloneNameValue = jVarLocalCloneName.value;
+
+    return {
+        folderName: jVarLocalFolderName,
+        inFileNameOnly: jVarLocalfilename,
+        inToFileName: jVarLocalCloneNameValue
+    }
+};
+
+export { StartFunc };
--- src/Projects/JSONAdminApi/Js/Columns/Tabular/Duplicate/DataDuplicate/DuplicateFles.js
+++ src/Projects/JSONAdminApi/Js/Columns/Tabular/Duplicate/DataDuplicate/DuplicateFles.js
@@ -1,10 +1,11 @@
-import { jFUpdateFunc } from "./DuplicateFiles/Addlisteners.js";
+// import { jFUpdateFunc } from "./DuplicateFiles/Addlisteners.js";
 import { jFCreateFoldersToDom } from "./DuplicateFiles/ShowOnDom.js";
 import ApiConfigJson from '../../ApiConfig.json' assert {type: 'json'};
-
+import { StartFunc as StartFuncClickFunc } from "./DuplicateFiles/DuplicateWithButton/1-ClickFunc.js";
 let jVarCommonProjectName = ApiConfigJson.ProjectName;
 
 
 jFCreateFoldersToDom({ inProjectName: jVarCommonProjectName }).then(FromjFCreateFoldersToDom => {
-    jFUpdateFunc({ inProjectName: jVarCommonProjectName });
+    // jFUpdateFunc({ inProjectName: jVarCommonProjectName });
+    StartFuncClickFunc();
 });
