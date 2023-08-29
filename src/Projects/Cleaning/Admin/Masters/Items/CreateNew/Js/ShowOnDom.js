be738a177683aea48209b383bacab8f53480b257
Satish Addanki <satishaddanki321@gmail.com>
2023-08-29 18:16:44 +0530

    satish

--- src/Projects/Cleaning/Admin/Masters/Items/CreateNew/Js/ShowOnDom.js
+++ src/Projects/Cleaning/Admin/Masters/Items/CreateNew/Js/ShowOnDom.js
@@ -1,8 +1,12 @@
 import { StartFunc as StartFuncFromLocalStorage } from "./FromLocalStorage/Items.js";
+import { StartFunc as StartFuncFetchFuncs } from "./FetchFuncs/Items/EntryFile.js";
 
-let StartFunc = async ({ inProjectName }) => {
+let StartFunc = async () => {
+
+    await StartFuncFetchFuncs();
+
     let jVarLocalDataNeeded = StartFuncFromLocalStorage();
-    
+
     if (jVarLocalDataNeeded !== null) {
         let jVarLocalRawTemplate = document.getElementById("HbsTemplateForFoldersOnly").innerHTML;
         document.getElementById("KCont1").innerHTML = Handlebars.compile(jVarLocalRawTemplate)(jVarLocalDataNeeded);
