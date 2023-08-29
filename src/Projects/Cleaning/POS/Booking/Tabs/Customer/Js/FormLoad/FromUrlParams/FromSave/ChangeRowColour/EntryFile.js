dbdb81cb728e542324b703b85c394d5b40e45951
Satish Addanki <satishaddanki321@gmail.com>
2023-08-29 13:34:01 +0530

    satish

---
+++ src/Projects/Cleaning/POS/Booking/Tabs/Customer/Js/FormLoad/FromUrlParams/FromSave/ChangeRowColour/EntryFile.js
@@ -0,0 +1,12 @@
+let StartFunc = ({inRowPk}) => {
+    let jVarLocalKCont1 = document.getElementById("KCont1");
+
+    let jVarLocalElementsToFocus = jVarLocalKCont1.querySelectorAll("[data-pk]");
+    console.log("jVarLocalElementsToFocus",jVarLocalElementsToFocus);
+                if (jVarLocalElementsToFocus.length > 0) {
+                    jVarLocalElementsToFocus[0].focus();
+                }
+
+};
+
+export { StartFunc };
