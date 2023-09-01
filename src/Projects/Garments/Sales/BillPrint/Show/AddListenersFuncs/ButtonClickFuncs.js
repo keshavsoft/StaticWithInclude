ca601a645a39d3c562c1221bc1d6a4e628196bed
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-01 18:51:24 +0530

    SREENIVAS

--- src/Projects/Garments/Sales/BillPrint/Show/AddListenersFuncs/ButtonClickFuncs.js
+++ src/Projects/Garments/Sales/BillPrint/Show/AddListenersFuncs/ButtonClickFuncs.js
@@ -1,5 +1,6 @@
 import { StartFunc as StartFuncEntry } from "./BlockPrintFiles/Entry.js";
 import { StartFunc as StartFuncEntry2 } from "./BlockPrintFiles 2/Entry.js";
+import { StartFunc as StartFuncEntry3 } from "./BlockPrintFiles 3/Entry.js";
 
 let StartFunc = () => {
     let LocalPrintId = document.getElementById("PrintId");
@@ -8,8 +9,8 @@ let StartFunc = () => {
     let LocalPrintId1 = document.getElementById("PrintId2");
     LocalPrintId1.addEventListener("click", StartFuncEntry2)
 
-    let LocalPrintId2 = document.getElementById("PrintId");
-    LocalPrintId2.addEventListener("click", StartFuncEntry)
+    let LocalPrintId2 = document.getElementById("PrintId3");
+    LocalPrintId2.addEventListener("click", StartFuncEntry3)
 
    
 };
