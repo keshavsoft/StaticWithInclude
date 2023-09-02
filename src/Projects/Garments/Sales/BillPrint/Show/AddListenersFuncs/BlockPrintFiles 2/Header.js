1104d5b2e0f92585c9faa6148f41dab8a1235958
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-02 13:32:26 +0530

    SREENIVAS

--- src/Projects/Garments/Sales/BillPrint/Show/AddListenersFuncs/BlockPrintFiles 2/Header.js
+++ src/Projects/Garments/Sales/BillPrint/Show/AddListenersFuncs/BlockPrintFiles 2/Header.js
@@ -7,20 +7,20 @@ const StartFunc = () => {
     let k3 = document.getElementById("TemplateForFirmHeadId");
 
      // k1.innerHTML += k3.innerHTML;
-     k1.innerHTML += `               <span class="inner-pre" style="font-size: 33px; font-weight: bold;">MAGUVA</span>\n`;
-     k1.innerHTML += `        50-50-30/12/2, vandana plaza,\n`
-     k1.innerHTML += `            GURUDWARA,VISHAKAPATNAM,\n`
-     k1.innerHTML += `    GSTIN:37ACJPL9300K2ZB,Ph:08912796666,\n`
-     k1.innerHTML += `                State Code:37\n`
+     k1.innerHTML += `               <span class="inner-pre" style="font-size: 33px; font-weight: ; font-family: Grobek Normal">MAGUVA</span>\n`;
+     k1.innerHTML += `     <span class="inner-pre" style="font-size: 16px;">50-50-30/12/2, vandana plaza,</span>\n`
+     k1.innerHTML += `            <span class="inner-pre" style="font-size: 16px;">GURUDWARA,VISHAKAPATNAM,</span>\n`
+     k1.innerHTML += `<span class="inner-pre" style="font-size: 16px;">GSTIN:37ACJPL9300K2ZB,Ph:08912796666,</span>\n`
+     k1.innerHTML += `                <span class="inner-pre" style="font-size: 16px;">State Code:37</span>\n`
      // k1.innerHTML += k2.innerHTML;
     k1.innerHTML += `---------------------------------------------\n`
     k1.innerHTML += `               <span class="inner-pre" style="font-size: 16px; font-weight: bold;">TAX INVOICE</span> \n`
     k1.innerHTML += `---------------------------------------------\n`
-    k1.innerHTML += `  <span class="inner-pre" style="font-size: 16px; font-weight: bold;">Customer :   ${jVarLocalBillDataAsJson.CustomerName}</span>\n`;
-    k1.innerHTML += `  <span class="inner-pre" style="font-size: 16px; font-weight: bold;">Phone    :   ${jVarLocalBillDataAsJson.CustomerNumber}</span>\n`;
+    k1.innerHTML += `<span class="inner-pre" style="font-size: 16px; font-weight: bold;">Customer :   ${jVarLocalBillDataAsJson.CustomerName}</span>\n`;
+    k1.innerHTML += `<span class="inner-pre" style="font-size: 16px; font-weight: bold;">Phone    :   ${jVarLocalBillDataAsJson.CustomerNumber}</span>\n`;
     k1.innerHTML += `----------------------------------------------\n`
-    k1.innerHTML += `  Bill : ${jVarLocalBillDataAsJson.BillNumber}   Date : ${jVarLocalBillDataAsJson.Date}\n`;
-    k1.innerHTML += `  TimeStamp : ${jVarLocalBillDataAsJson.DateTime}\n`;
+    k1.innerHTML += `<span class="inner-pre" style="font-size: 16px;">Bill : ${jVarLocalBillDataAsJson.BillNumber}   Date : ${jVarLocalBillDataAsJson.Date} </span>\n`;
+    k1.innerHTML += `<span class="inner-pre" style="font-size: 16px;">TimeStamp : ${jVarLocalBillDataAsJson.DateTime}</span>\n`;
 
 };
 
