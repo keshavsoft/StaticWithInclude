cf7da84ec4f14061dcd78bb4c29999048ce7174c
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-07 16:31:59 +0530

    SREENIVAS

--- src/Projects/JSONApi/Html/UserData/Menu/WithBSTable/UIEntry/Js/Addlistioners/Entry.js
+++ src/Projects/JSONApi/Html/UserData/Menu/WithBSTable/UIEntry/Js/Addlistioners/Entry.js
@@ -1,6 +1,8 @@
+import { StartFunc as StartFuncBSVerticalIdButton } from "./ButtonClickFuncs/BSVerticalIdButton.js";
 import { StartFunc as StartFuncBSTableIdButton } from "./ButtonClickFuncs/BSTableIdButton.js";
 import { StartFunc as StartFuncBSTreeIdButton } from "./ButtonClickFuncs/BSTreeIdButton.js";
 const StartFunc = () => {
+    StartFuncBSVerticalIdButton();
     StartFuncBSTableIdButton();
     StartFuncBSTreeIdButton();
 };
