6b1a7a44a7891ff0c7cb327c8c50ff6580198365
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-07 13:44:43 +0530

    SREENIVAS

--- src/Projects/JSONApi/Html/UserData/Menu/WithBSTable/UIEntry/Js/Addlistioners/Entry.js
+++ src/Projects/JSONApi/Html/UserData/Menu/WithBSTable/UIEntry/Js/Addlistioners/Entry.js
@@ -1,9 +1,19 @@
 const StartFunc = () => {
-    let jVarLocalHtmlId = 'localBSTableId';
-    let jVarLocallocalBSTableId = document.getElementById(jVarLocalHtmlId);
+    let jVarLocalHtmlId = 'BSTableId';
+    let jVarlocalBSTableId = document.getElementById(jVarLocalHtmlId);
 
-    jVarLocallocalBSTableId.addEventListener("click", () => {
+    jVarlocalBSTableId.addEventListener("click", (event) => {
+        jFLocalToURL();
+
     });
+};
+let jFLocalToURL = () => {
+
+    const url = new URL(window.location.href);
+    const params1 = new URLSearchParams(url.search);
+    let NewURl = new URL("../UI/VerticalSimple/VerticalSimple.html", url);
+    const new_url = new URL(`${NewURl.href}?${params1}`);
+    window.location.href = new_url.href;
 };
 
 export { StartFunc };
