4da45b2823e445d227f9ceec869288fe8c7778a0
Satish Addanki <satishaddanki321@gmail.com>
2023-09-08 21:13:15 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/Kakinada/QrCodeTree/AddListeners/GoButtonScan/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/Kakinada/QrCodeTree/AddListeners/GoButtonScan/2-ButtonClickFunc.js
@@ -1,35 +1,35 @@
-// import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
-// import { StartFunc as StartFuncPreparePostData } from "./4-PreparePostData.js";
-// import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
-// import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
+import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
+import { StartFunc as StartFuncPreparePostData } from "./4-PreparePostData.js";
+import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
+import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
 
 let StartFunc = async () => {
    
-    // if (StartFuncCheckBeforeFetch()) {
-    //     let jVarLocalBodyData = StartFuncPreparePostData();
-    //     console.log("jVarLocalBodyData", jVarLocalBodyData);
+    if (StartFuncCheckBeforeFetch()) {
+        let jVarLocalBodyData = StartFuncPreparePostData();
+        console.log("jVarLocalBodyData", jVarLocalBodyData);
 
-    //     let response = await StartFuncFetchFunc({
-    //         inBodyData: jVarLocalBodyData
-    //     });
+        let response = await StartFuncFetchFunc({
+            inBodyData: jVarLocalBodyData
+        });
 
-        // StartFuncAfterFetch({ inFromFetch: response });
-    // };
-    let QrCodeNumber = jFLocalFromDomScanId();
+        StartFuncAfterFetch({ inFromFetch: response });
+    };
+    // let QrCodeNumber = jFLocalFromDomScanId();
 
-    const myUrlWithParams = new URL(`${window.location.origin}${window.location.pathname}`);
+    // const myUrlWithParams = new URL(`${window.location.origin}${window.location.pathname}`);
 
-    myUrlWithParams.searchParams.append("Qrcode", QrCodeNumber);
+    // myUrlWithParams.searchParams.append("Qrcode", QrCodeNumber);
 
-    window.location.href = myUrlWithParams.href;
+    // window.location.href = myUrlWithParams.href;
 
 };
 
-let jFLocalFromDomScanId = () => {
-    let jVarLocalHtmlScanId = 'ScanId';
-   let jVarHtmlScanId = document.getElementById(jVarLocalHtmlScanId);
-   let jVarHtmlScanIdValue = jVarHtmlScanId.value.trim();
-   return jVarHtmlScanIdValue;
-};
+// let jFLocalFromDomScanId = () => {
+//     let jVarLocalHtmlScanId = 'ScanId';
+//    let jVarHtmlScanId = document.getElementById(jVarLocalHtmlScanId);
+//    let jVarHtmlScanIdValue = jVarHtmlScanId.value.trim();
+//    return jVarHtmlScanIdValue;
+// };
 
 export { StartFunc };
