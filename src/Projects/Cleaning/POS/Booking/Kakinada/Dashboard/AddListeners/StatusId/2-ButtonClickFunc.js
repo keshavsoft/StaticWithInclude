a90b56489f031e59ed758606c3f9434a7c9067ea
keshavsoft <nknnkumar@yahoo.com>
2023-09-11 22:32:34 +0530

    keshav

--- src/Projects/Cleaning/POS/Booking/Kakinada/Dashboard/AddListeners/StatusId/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/Kakinada/Dashboard/AddListeners/StatusId/2-ButtonClickFunc.js
@@ -1,11 +1,13 @@
 import { StartFunc as StartFuncNewCustomers } from "./FetchFuncs/NewCustomers/EntryFile.js";
 import { StartFunc as StartFuncFrequentCustomers } from "./FetchFuncs/FrequentCustomers/EntryFile.js";
 import { StartFunc as StartFuncTodayCustomers } from "./FetchFuncs/TodayCustomers/EntryFile.js";
+import { StartFunc as StartFuncLatestOrders } from "./FetchFuncs/LatestOrders/EntryFile.js";
 
 let StartFunc = () => {
     StartFuncNewCustomers();
     StartFuncFrequentCustomers();
     StartFuncTodayCustomers();
+    StartFuncLatestOrders();
 };
 
 export { StartFunc };
