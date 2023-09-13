4fddf0153d3288e477cda26b62defa80484c8b55
Satish Addanki <satishaddanki321@gmail.com>
2023-09-13 13:33:25 +0530

    satish

---
+++ src/Projects/JSONApi/Factory/FolderFile/Config/DataFolder/Items/BSTable/Js/FetchFuncs/FetchHeaders/EntryFile.js
@@ -0,0 +1,13 @@
+import KeysJson from './Keys.json' assert {type: 'json'};
+// import BodyKeysJson from './BodyKeys.json' assert {type: 'json'};
+import BodyKeysJson from "../../../../ConfigKeys/FetchKeys/ForPostKeys.json" assert {type: 'json'};
+
+let StartFunc = () => {
+    let jVarLocalBodyKeysJson = BodyKeysJson;
+    
+    KeysJson.body = JSON.stringify(jVarLocalBodyKeysJson);
+
+    return KeysJson;
+};
+
+export { StartFunc }
