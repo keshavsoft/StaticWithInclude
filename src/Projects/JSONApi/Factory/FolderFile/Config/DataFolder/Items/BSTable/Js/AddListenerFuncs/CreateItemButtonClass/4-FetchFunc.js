2581e4822028f138a213e9b43742c5d66dd37177
Satish Addanki <satishaddanki321@gmail.com>
2023-09-13 17:37:17 +0530

    satish

--- src/Projects/JSONApi/Factory/FolderFile/Config/DataFolder/Items/BSTable/Js/AddListenerFuncs/CreateItemButtonClass/4-FetchFunc.js
+++ src/Projects/JSONApi/Factory/FolderFile/Config/DataFolder/Items/BSTable/Js/AddListenerFuncs/CreateItemButtonClass/4-FetchFunc.js
@@ -3,7 +3,7 @@ import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js"
 
 let StartFunc = async () => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    let jVarLocalFetchUrl = "/JSONAdminApi/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/ItemName/CreateNew/CreateItem"
+    let jVarLocalFetchUrl = "/JSONAdminApi/AdminApi/AsTree/Json/UserFolders/DataFolder/FileInFolder/ItemName/InsertNew/InsertNew"
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
     let data = await response.json();
     return await data;
--- src/Projects/JSONApi/Factory/FolderFile/Config/DataFolder/Items/BSTable/Js/BSTable.js
+++ src/Projects/JSONApi/Factory/FolderFile/Config/DataFolder/Items/BSTable/Js/BSTable.js
@@ -1,11 +1,12 @@
 import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 import { StartFunc as StartFuncAfterDomLoad } from "./AfterDomLoad/StartFunc.js";
-
+import { StartFunc as StartFuncAddlisteners } from "./AddListenerFuncs/StartFunc.js";
 const StartFunc = () => {
     let jVarLocalFromAdmin = true;
 
     if (jVarLocalFromAdmin) {
+        StartFuncAddlisteners();
         StartFuncFormLoad();
 
         StartFuncShowOnDom({}).then(() => {
