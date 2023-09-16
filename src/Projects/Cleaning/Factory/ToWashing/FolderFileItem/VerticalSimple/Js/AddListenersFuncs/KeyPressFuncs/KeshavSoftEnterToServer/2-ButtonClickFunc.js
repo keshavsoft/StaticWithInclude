02fb8b16a479d31637f99fe178bfab1e1c8f73be
Satish Addanki <satishaddanki321@gmail.com>
2023-09-16 12:25:03 +0530

    satish

--- src/Projects/Cleaning/Factory/ToWashing/FolderFileItem/VerticalSimple/Js/AddListenersFuncs/KeyPressFuncs/KeshavSoftEnterToServer/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/Factory/ToWashing/FolderFileItem/VerticalSimple/Js/AddListenersFuncs/KeyPressFuncs/KeshavSoftEnterToServer/2-ButtonClickFunc.js
@@ -1,43 +1,24 @@
 import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
 import { StartFunc as StartFuncPreparePostData } from "./4-PreparePostData.js";
-// import { StartFunc as StartFuncPreparePostData } from "../../../FetchFuncs/FetchHeaders/EntryFile.js";
-// import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
-// import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
+import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
+import { StartFunc as StartFuncAfterFetch } from "./6-AfterFetch.js";
 
 let StartFunc = async ({ inEvent }) => {
     if (inEvent.keyCode === 13) {
         let jVarLocalCurrentTarget = inEvent.currentTarget;
         let jVarLocalCurrentName = jVarLocalCurrentTarget.name;
-        let pk = jFLocalFromDompk();
 
-        let LocalArray = Object.values(jVarGlobalPresentFetchData.JsonData);
-        let key = LocalArray.filter((e) => e.pk === pk);
+        let jVarLocalHtmlCard = jVarLocalCurrentTarget.closest(".card");
+        if (StartFuncCheckBeforeFetch()) {
+            let jVarLocalBodyData = StartFuncPreparePostData({ inEvent });
 
-        jFLocalToInputItemName({ inItemName: key[0].ItemName })
-        // let jVarLocalHtmlCard = jVarLocalCurrentTarget.closest(".card");
-        // if (StartFuncCheckBeforeFetch()) {
-        //     let jVarLocalBodyData = StartFuncPreparePostData({ inEvent });
+            let response = await StartFuncFetchFunc({
+                inBodyData: jVarLocalBodyData
+            });
 
-        //     let response = await StartFuncFetchFunc({
-        //         inBodyData: jVarLocalBodyData
-        //     });
-
-        // StartFuncAfterFetch({ FetchData: response, inHtmlCard: jVarLocalHtmlCard, inCurrentName: jVarLocalCurrentName });
-        // };
+            StartFuncAfterFetch({ FetchData: response, inHtmlCard: jVarLocalHtmlCard, inCurrentName: jVarLocalCurrentName });
+        };
     };
-};
-
-let jFLocalFromDompk = () => {
-    let jVarLocalHtmlpk = 'pk';
-    let jVarHtmlpk = document.getElementById(jVarLocalHtmlpk);
-    let jVarHtmlpkValue = jVarHtmlpk.value.trim();
-    return jVarHtmlpkValue;
-};
-
-let jFLocalToInputItemName = ({ inItemName }) => {
-    let jVarLocalHtmlId = 'ItemName';
-    let jVarLocalItemName = document.getElementById(jVarLocalHtmlId);
-    jVarLocalItemName.value = inItemName;
 };
 
 export { StartFunc };
--- src/Projects/Cleaning/Factory/ToWashing/FolderFileItem/VerticalSimple/Js/FetchFuncs/PostFetch.js
+++ src/Projects/Cleaning/Factory/ToWashing/FolderFileItem/VerticalSimple/Js/FetchFuncs/PostFetch.js
@@ -3,7 +3,8 @@ import ApiConfigJson from "../../../ConfigKeys/ProjectKeys.json" assert {type: '
 
 let StartFunc = async () => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    let jVarLocalFetchUrl = `/${ApiConfigJson.ProjectName}/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsObjectWithPK`;
+    let jVarLocalFetchUrl = `/${ApiConfigJson.ProjectName}/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Vertical/HtmlCreate/Fromjson`;
+
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
     let data = await response.json();
 
