281dbe65352d3e8616ad1dbe59e9c356c5135fd1
Satish Addanki <satishaddanki321@gmail.com>
2023-09-19 19:45:34 +0530

    satish

--- src/Projects/JSONApi/Factory/FolderFileItem/CardMenu/Files/Js/FormLoad/FetchFuncs/AfterFetch/EntryFile.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/CardMenu/Files/Js/FormLoad/FetchFuncs/AfterFetch/EntryFile.js
@@ -1,8 +1,9 @@
 let StartFunc = ({ inFetchData }) => {
-    console.log("inFetchData",inFetchData);
-    LocalStartFunc({ inFilesObjects : inFetchData.Folders})
+    console.log("inFetchData", inFetchData);
+
+    LocalStartFunc({ inFilesObjects: inFetchData })
     // let jVarCardBody = document.getElementById("KCont1");
-    
+
     // if (inFetchData.KTF) {
     //     jVarGlobalPresentViewData = KeshavSoftCrud.BuildFromArray(inFetchData.DataFromServer);
     //     jVarGlobalPresentViewData[0].HTMLControlType = "VerticalSimple";
@@ -18,11 +19,12 @@ let LocalStartFunc = ({ inFilesObjects }) => {
     let jVarLocalKCont1 = document.getElementById("KCont1");
     let jVarLocalNewRow = document.createElement("div");
     jVarLocalNewRow.setAttribute("class", "row");
-
+    let jVarLocalFolderName = getUrlQueryParams({ inGetKey: "inFolderName" });
     Object.entries(jVarLocalFilesObjects).forEach(
         ([key, value]) => {
             let jVarLocalFromLoop = LocalLoopFunc({
-                inFolderName:  key,
+                inFolderName: jVarLocalFolderName,
+                inFileName: value.FileName
             });
             jVarLocalNewRow.appendChild(jVarLocalFromLoop);
         });
@@ -32,13 +34,21 @@ let LocalStartFunc = ({ inFilesObjects }) => {
 
 };
 
-let LocalLoopFunc = ({ inFolderName }) => {
-    console.log("inFolderName99",inFolderName);
-    let jVarLocalTemplate = document.getElementById("TemplateForFolders");
+let LocalLoopFunc = ({ inFolderName, inFileName }) => {
+    console.log("inFolderName99", inFolderName);
+    let jVarLocalTemplate = document.getElementById("TemplateForFiles");
     var jVarLocalTemplateClone = jVarLocalTemplate.cloneNode(true);
     jVarLocalTemplateClone.innerHTML = jVarLocalTemplateClone.innerHTML.replace(/{{FolderName}}/g, inFolderName);
+    jVarLocalTemplateClone.innerHTML = jVarLocalTemplateClone.innerHTML.replace(/{{FileName}}/g, inFileName);
 
     return document.importNode(jVarLocalTemplateClone.content, true);
+};
+
+let getUrlQueryParams = ({ inGetKey }) => {
+    const queryString = window.location.search;
+    const parameters = new URLSearchParams(queryString);
+    const value = parameters.get(inGetKey);
+    return value;
 };
 
 export { StartFunc }
