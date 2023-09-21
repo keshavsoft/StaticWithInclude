450c5a191031dad20627d4b950df7a23e176b4ff
Keshav <nknnkumar@yahoo.com>
2023-09-21 09:43:14 +0530

    keshav

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/AddListeners/StatusId/FetchFuncs/LatestOrders/6-AfterFetch.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/AddListeners/StatusId/FetchFuncs/LatestOrders/6-AfterFetch.js
@@ -1,35 +1,38 @@
 let StartFunc = ({ inFromFetch }) => {
     let jVarLocalData = inFromFetch.JsonData;
-    console.log("inFromFetch9899", jVarLocalData);
-    // var $table = $('#NewCustomersTable')
-    // $table.bootstrapTable({ data: jVarLocalData });
     var template = Handlebars.compile(jFLocalFromDomLatestOrdersTableTemplateRow());
-    console.log("hi",template);
+
     let jVarLocalHtmlId = 'LatestOrdersTable';
     let jVarLocalLatestOrdersTable = document.getElementById(jVarLocalHtmlId);
-    console.log("jVarLocalLatestOrdersTable",jVarLocalLatestOrdersTable);
-    console.log("jVarLocalNewCustomersTable",jVarLocalLatestOrdersTable);
-    jVarLocalLatestOrdersTable.querySelector('tbody').innerHTML = template(jFLocalInsertAggValues({inData: jVarLocalData}));
-
+    let jVarLocalTransformedData = jFLocalInsertAggValues({ inData: jVarLocalData });
+    console.log("jVarLocalTransformedData : ", jVarLocalTransformedData);
+    jVarLocalLatestOrdersTable.querySelector('tbody').innerHTML = template(jVarLocalTransformedData);
 };
 
 let jFLocalFromDomLatestOrdersTableTemplateRow = () => {
     let jVarLocalHtmlLatestOrdersTableTemplateRow = 'LatestOrdersTemplateRow';
-   let jVarLatestOrdersTableTemplateRow = document.getElementById(jVarLocalHtmlLatestOrdersTableTemplateRow);
-   let jVarLatestOrdersTableTemplateRowValue = jVarLatestOrdersTableTemplateRow.innerHTML.trim();
-   return jVarLatestOrdersTableTemplateRowValue;
+    let jVarLatestOrdersTableTemplateRow = document.getElementById(jVarLocalHtmlLatestOrdersTableTemplateRow);
+    let jVarLatestOrdersTableTemplateRowValue = jVarLatestOrdersTableTemplateRow.innerHTML.trim();
+    return jVarLatestOrdersTableTemplateRowValue;
 };
 
 let jFLocalInsertAggValues = ({ inData }) => {
-    let jVarLocalReturnObject = {};
+    let jVarLocalReturnObject = [];
 
-    jVarLocalReturnObject = Object.fromEntries(Object.entries(inData).map(element => {
+    jVarLocalReturnObject = Object.entries(inData).map(element => {
         element[1].AggValues = {};
         element[1].AggValues.ItemDetails = `${Object.keys(element[1].ItemsInOrder).length} / ${Object.values(element[1].ItemsInOrder).map(p => p.Pcs).reduce((acc, val) => acc + val)}`;
         element[1].AggValues.SettlementAmount = element[1].CheckOutData.CardAmount + element[1].CheckOutData.CashAmount + element[1].CheckOutData.UPIAmount;
+        element[1].IsSettled = false;
 
-        return element;
-    }));
+        if (Object.keys(element[1].CheckOutData).length > 0) {
+            element[1].IsSettled = true;
+        };
+        // CheckOutData
+
+
+        return element[1];
+    });
 
     return jVarLocalReturnObject;
 };
