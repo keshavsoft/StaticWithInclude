a966a40d0fc69bdce0b89ae1efb1d09bb060225b
Satish Addanki <satishaddanki321@gmail.com>
2023-09-22 15:33:35 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/QrCodeMenu/FromOrders/Js/FormLoad/FetchFuncs/FetchHeaders/EntryFile.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/QrCodeMenu/FromOrders/Js/FormLoad/FetchFuncs/FetchHeaders/EntryFile.js
@@ -6,9 +6,28 @@ let StartFunc = () => {
     let jVarLocalReferncePk = getUrlQueryParams({ inGetKey: "OrderNumber" });
     let jVarLocalBranchName = getUrlQueryParams({ inGetKey: "Branch" });
 
+    if (jVarLocalReferncePk === null ) {
+        Swal.fire({
+            title: 'OrderNumber Not Found',
+            text: "OrderNumber Not Found in Params",
+            icon: 'error',
+        });
+        return;
+    };
+    if (jVarLocalBranchName === null ) {
+        Swal.fire({
+            title: 'Branch Not Found',
+            text: "Branch Not Found in Params",
+            icon: 'error',
+        });
+        return;
+    };
+
+    jFLocalToInnerHtmlOrderNumberTextId({ inOrderNumberTextId: jVarLocalReferncePk });
+    jFLocalToInnerHtmlBranchNameTextId({ inBranchNameTextId: jVarLocalBranchName })
     // jVarLocalBodyKeysJson.FilterString=`value.GenerateReference.ReferncePk === '${jVarLocalReferncePk}'`;
-    jVarLocalBodyKeysJson.FilterString = jVarLocalBodyKeysJson.FilterString.replace("{{OrderNumber}}",jVarLocalReferncePk);
-    jVarLocalBodyKeysJson.FilterString = jVarLocalBodyKeysJson.FilterString.replace("{{Branch}}",jVarLocalBranchName);
+    jVarLocalBodyKeysJson.FilterString = jVarLocalBodyKeysJson.FilterString.replace("{{OrderNumber}}", jVarLocalReferncePk);
+    jVarLocalBodyKeysJson.FilterString = jVarLocalBodyKeysJson.FilterString.replace("{{Branch}}", jVarLocalBranchName);
 
     KeysJson.body = JSON.stringify(jVarLocalBodyKeysJson);
 
@@ -22,5 +41,16 @@ let getUrlQueryParams = ({ inGetKey }) => {
     return value;
 };
 
+let jFLocalToInnerHtmlOrderNumberTextId = ({ inOrderNumberTextId }) => {
+    let jVarLocalHtmlId = 'OrderNumberTextId';
+    let jVarLocalOrderNumberTextId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalOrderNumberTextId.innerHTML = inOrderNumberTextId;
+};
+
+let jFLocalToInnerHtmlBranchNameTextId = ({ inBranchNameTextId }) => {
+    let jVarLocalHtmlId = 'BranchNameTextId';
+    let jVarLocalBranchNameTextId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalBranchNameTextId.innerHTML = inBranchNameTextId;
+};
 
 export { StartFunc }
---
+++ src/Projects/Cleaning/POS/Booking/Vzm/QrCodeMenu/QrCodeTree/Js/ForTree/EntryFileForObject.js
@@ -0,0 +1,62 @@
+import { StartFunc as StartFuncForObject } from "./ForObject.js";
+import { StartFunc as StartFuncForCollapse } from "./ForCollapse.js";
+
+let StartFunc = ({ inFromFetch }) => {
+    let k1 = document.getElementById("KCont1");
+    k1.innerHTML = "";
+    let jVarLocalData = inFromFetch.JsonData;
+    let jVarLocalMainUl = document.createElement("ul");
+    jVarLocalMainUl.classList.add("list-unstyled");
+    k1.appendChild(jVarLocalMainUl);
+    StartFuncForObject({ inParentUl: jVarLocalMainUl, inObject: jVarLocalData });
+
+    StartFuncForCollapse();
+    let jVarLocalHtmlId = 'TextId';
+    let jVarLocalTextId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalTextId.innerHTML = jFLocalFromDomScanId();
+    jFLocalHtmlFocusScanId();
+    jFLocalToInputScanId({ inScanId: "" });
+};
+
+let jFLocalFromDomScanId = () => {
+    let jVarLocalHtmlScanId = 'ScanId';
+   let jVarHtmlScanId = document.getElementById(jVarLocalHtmlScanId);
+   let jVarHtmlScanIdValue = jVarHtmlScanId.value.trim();
+   return jVarHtmlScanIdValue;
+};
+
+let jFLocalHtmlFocusScanId = () => {
+    let jVarLocalHtmlId = 'ScanId';
+    let jVarLocalScanId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalScanId.focus();
+};
+
+let jFLocalToInputScanId = ({ inScanId }) => {
+    let jVarLocalHtmlId = 'ScanId';
+    let jVarLocalScanId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalScanId.value = inScanId;
+};
+
+let data = {
+    key1: "value1",
+    key2: {
+        Name: "Keshav",
+        Family: {
+            Wife: "Purna",
+            Children: {
+                Big: "Chakri"
+            }
+        }
+    },
+    key3: "value3"
+};
+
+let data1 = {
+    key1: "value1",
+    key2: "Keshav",
+    key3: "value3"
+};
+
+export { StartFunc }
+
+
