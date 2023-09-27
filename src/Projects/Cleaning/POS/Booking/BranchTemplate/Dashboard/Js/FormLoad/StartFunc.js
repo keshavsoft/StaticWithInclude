107794342b9cc4700c1d7db18126ea9a6d200369
Satish Addanki <satishaddanki321@gmail.com>
2023-09-27 15:47:59 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/Js/FormLoad/StartFunc.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/Js/FormLoad/StartFunc.js
@@ -1,14 +1,13 @@
 import { StartFunc as StartFuncLoadToDataList } from "./Customers/LoadToDataList.js";
 import { StartFunc as StartFuncFetchFuncs } from "./FetchFuncs/EntryFile.js";
 import { StartFunc as StartFuncFromUrlParams } from "./FromUrlParams/EntryFile.js";
-import { StartFunc as StartFuncBranchNameId } from "./BranchNameId/StartFunc.js";
+
 const StartFunc = () => {
     StartFuncFromUrlParams();
 
     StartFuncFetchFuncs().then(() => {
         StartFuncLoadToDataList()
     });
-    StartFuncBranchNameId();
 };
 
 export { StartFunc };
