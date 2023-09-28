1bc136becd934127b3671b250093b9065402cc95
Satish Addanki <satishaddanki321@gmail.com>
2023-09-28 20:40:12 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/ToFactory/DC/BSTableFromScreen/Js/FetchFuncs/AfterFetch/EntryFile.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/ToFactory/DC/BSTableFromScreen/Js/FetchFuncs/AfterFetch/EntryFile.js
@@ -1,11 +1,49 @@
 let StartFunc = ({ inDataToShow }) => {
     let jVarLocalDataToShow = inDataToShow;
-
-    jFLocalPrepareHeaderForDataOnly({ data: jVarLocalDataToShow })
+    // jFLocalPrepareHeaderForDataOnly({ data: jVarLocalDataToShow })
     var $table = $('#table');
-    $table.bootstrapTable({ data: jVarLocalDataToShow.DataFromServer[0].KData.TableData });
+    let jVarLocalColumns = [];
+    jVarLocalColumns.push({
+        field: 'pk',
+        title: 'pk'
+    });
+    jVarLocalColumns.push({
+        field: 'VoucherNumber',
+        title: 'VoucherNumber'
+    });
+    jVarLocalColumns.push({
+        field: 'Date',
+        title: 'Date'
+    });
+    jVarLocalColumns.push({
+        field: 'Description',
+        title: 'Description',
+        checkbox: true
+    });
+    jVarLocalColumns.push({
+        field: 'Description',
+        title: 'Description',
+        formatter: operateFormatter
+    });
+    // $table.bootstrapTable({ data: jVarLocalDataToShow.DataFromServer[0].KData.TableData });
+    $table.bootstrapTable({
+        data: jVarLocalDataToShow.DataFromServer[0].KData.TableData,
+        columns: jVarLocalColumns
+    });
 
 };
+function operateFormatter(value, row, index) {
+    console.log("pk",value, row, index);
+    return [
+        `<a class="like" href="../../Scan/VerticalFromConfigJson/VerticalFromConfigJson.html?VoucherRef=${row.pk}" title="Like">`,
+        '<i class="fa fa-heart">Save</i>',
+        '</a>  ',
+        '<a class="remove" href="javascript:void(0)" title="Remove">',
+        '<i class="fa fa-trash"></i>',
+        '</a>'
+    ].join('')
+};
+
 
 let jFLocalPrepareHeaderForDataOnly = ({ data }) => {
     console.log("data", data);
