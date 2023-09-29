30c76ec524cf7b06e43729fc9faf4b905d84251a
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-29 19:05:14 +0530

    SREENIVAS

--- src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BSTableFromScreen/Js/BSTableFromScreen.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BSTableFromScreen/Js/BSTableFromScreen.js
@@ -1,4 +1,4 @@
-import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
+import { StartFunc as StartFuncShowOnDom } from "./Entry.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 import { StartFunc as StartFuncAfterDomLoad } from "./AfterDomLoad/StartFunc.js";
 
