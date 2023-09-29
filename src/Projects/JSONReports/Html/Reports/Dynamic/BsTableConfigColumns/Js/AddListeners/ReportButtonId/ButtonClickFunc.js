fe7a4b60180f7d7001e43162c6c97f9d34cbd09b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-29 17:05:39 +0530

    SREENIVAS

---
+++ src/Projects/JSONReports/Html/Reports/Dynamic/BsTableConfigColumns/Js/AddListeners/ReportButtonId/ButtonClickFunc.js
@@ -0,0 +1,21 @@
+import { StartFunc as StartFuncPrepareFetchUrl } from './PrepareFetchUrl.js'
+import { StartFunc as StartFuncFetchFunc } from './FetchFunc.js'
+// import { StartFunc as StartFuncAfterFetch } from './AfterFetch.js'
+import { StartFunc as StartFuncAfterFetch } from './AfterFetch/EntryFile.js'
+
+let StartFunc = async () => {
+    let jVarLocalHtmlId = 'ReportButtonShowProcessId';
+    let jVarLocalReportButtonShowProcessId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalReportButtonShowProcessId.style.display = "";
+
+    let jVarLocalFetchUrl = StartFuncPrepareFetchUrl(event);
+
+
+    let LocalResponseAsJson = await StartFuncFetchFunc({ inFetchUrl: jVarLocalFetchUrl });
+
+    StartFuncAfterFetch();
+
+    jVarLocalReportButtonShowProcessId.style.display = "none";
+};
+
+export { StartFunc }
