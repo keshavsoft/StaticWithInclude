39c0524b4ee198ab78cea3d625d69b7444dd3426
keshavsoft <nknnkumar@yahoo.com>
2023-09-29 13:34:34 +0530

    keshav

--- src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BSTableFromScreen/Js/BSTableFromScreen.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BSTableFromScreen/Js/BSTableFromScreen.js
@@ -8,7 +8,7 @@ const StartFunc = () => {
     if (jVarLocalFromAdmin) {
         StartFuncFormLoad();
 
-        StartFuncShowOnDom({}).then(() => {
+        StartFuncShowOnDom().then(() => {
             StartFuncAfterDomLoad();
         });
     };
