e6677ba21d39ae3e4f45c565b35fb430389c3e0b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-30 20:08:05 +0530

    SREENIVAS

--- src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BsTableFiltersFromScreen/Js/AddListeners/FilterDataId/EntryFile.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BsTableFiltersFromScreen/Js/AddListeners/FilterDataId/EntryFile.js
@@ -1,8 +1,8 @@
 import { StartFunc as StartFuncButtonClickFunc } from "./ButtonClickFunc/EntryFile.js";
 
-let StartFunc = () => {
+let StartFunc = async () => {
     let jVarLocalFilerButton = document.getElementById("FilterDataId");
-    jVarLocalFilerButton.addEventListener("click", StartFuncButtonClickFunc);
+    jVarLocalFilerButton.addEventListener("click", await StartFuncButtonClickFunc);
 };
 
 export { StartFunc }
