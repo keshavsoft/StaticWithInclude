1af7faba76f0fb49f5cbcc919d4c4cb7fa0936c7
Satish Addanki <satishaddanki321@gmail.com>
2023-10-02 15:14:14 +0530

    satish

--- src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BSTableFromScreen/Js/Entry.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/BSTableSubMenu/BSTableFromScreen/Js/Entry.js
@@ -1,9 +1,12 @@
-import { StartFunc as StartFuncFetchFoamFuncs } from "./FetchFoamFuncs/Entry.js";
+// import { StartFunc as StartFuncFetchFoamFuncs } from "./FetchFoamFuncs/Entry.js";
 
 import { StartFunc as StartFuncABreadcrumb } from "./Breadcrumb/Entery.js";
 
 let StartFunc = async () => {
-    StartFuncFetchFoamFuncs();
+    let jVarLocalHtmlId = 'RefreshBSTableId';
+    let jVarLocalRefreshBSTableId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalRefreshBSTableId.click();
+    // StartFuncFetchFoamFuncs();
     StartFuncABreadcrumb();
 };
 
