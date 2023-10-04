0fd306da891d8e8f852c6eebcadfedf9a80be4c6
Satish Addanki <satishaddanki321@gmail.com>
2023-10-04 17:19:50 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/QrCodeMenu/BSTable/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/Entry.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/QrCodeMenu/BSTable/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/Entry.js
@@ -4,17 +4,11 @@ import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
 let StartFunc = async () => {
     let jVarLocalDataNeeded = await StartFuncFetchFuncs();
-
+    
     if (jVarLocalDataNeeded !== null) {
         if (jVarLocalDataNeeded.KTF) {
             StartFuncAfterFetch({ inDataToShow: jVarLocalDataNeeded.JsonData });
-        } else {
-            Swal.fire({
-                title: 'Error',
-                text: `${jVarLocalDataNeeded.KReason}`,
-                icon: 'error',
-            });
-        }
+        };
     };
 };
 
