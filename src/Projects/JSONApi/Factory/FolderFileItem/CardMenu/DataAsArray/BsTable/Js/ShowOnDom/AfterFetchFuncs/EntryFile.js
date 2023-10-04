befcbfb1730c4a3d939fa5338b689599d5917763
keshavsoft <nknnkumar@yahoo.com>
2023-10-04 13:36:28 +0530

    keshav

--- src/Projects/JSONApi/Factory/FolderFileItem/CardMenu/DataAsArray/BsTable/Js/ShowOnDom/AfterFetchFuncs/EntryFile.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/CardMenu/DataAsArray/BsTable/Js/ShowOnDom/AfterFetchFuncs/EntryFile.js
@@ -1,13 +1,10 @@
 import { StartFunc as jFLocalPrepareHeaderForDataOnly } from "./TableColumns.js";
-// import { StartFunc as DeleteEntry } from "../../AddListeners/Entry.js";
-let StartFunc =async ({ inDataToShow }) => {
-    // console.log("inDataToShow:", inDataToShow);
+import { StartFunc as DeleteEntry } from "../../AddListeners/Entry.js";
 
+let StartFunc = async ({ inDataToShow }) => {
     let localmapData = inDataToShow.map((ele) => {
         return { ...ele.DataConfig, ...ele.Data }
     });
-    // console.log('localmapData::', localmapData);
-
 
     let jVarLocalDataToShow = localmapData;
     var $table = $('#table');
@@ -16,47 +13,8 @@ let StartFunc =async ({ inDataToShow }) => {
         data: jVarLocalDataToShow,
         columns: jFLocalPrepareHeaderForDataOnly({ data: jVarLocalDataToShow })
     });
-    // DeleteEntry();
+
+    DeleteEntry();
 };
 
-
-// let jFLocalPrepareHeaderForDataOnly = ({ data }) => {
-//     let jVarLocalColumns =Object.keys( data[0]);
-
-//     console.log("jVarLocalColumns::", jVarLocalColumns);
-
-
-//     let jVarLocalReturnArray = [];
-//     jVarLocalReturnArray = jVarLocalColumns.map(element => {
-//         return {
-//             title: element,
-//             field: element
-//         };
-//     });
-
-//     jVarLocalReturnArray.push(
-//         {
-//             field: 'operate',
-//             title: 'Item Operate',
-//             align: 'center',
-//             clickToSelect: false,
-//             events: window.operateEvents,
-//             formatter: operateFormatter
-//         });
-
-//     return jVarLocalReturnArray;
-// };
-
-// function operateFormatter(value, row, index) {
-//     return [
-//       '<a class="like" href="javascript:void(0)" title="Like">',
-//       '<i class="fa fa-heart"></i>',
-//       '</a>  ',
-//       '<a class="remove" href="javascript:void(0)" title="Remove">',
-//       '<i class="fa fa-trash"></i>',
-//       '</a>'
-//     ].join('')
-//   }
-
-
-export { StartFunc }
+export { StartFunc };
--- src/Projects/JSONApi/Factory/FolderFileItem/CardMenu/DataAsArray/BsTable/Js/ShowOnDom/Entry.js
+++ src/Projects/JSONApi/Factory/FolderFileItem/CardMenu/DataAsArray/BsTable/Js/ShowOnDom/Entry.js
@@ -2,7 +2,6 @@ import { StartFunc as StartFuncEntry } from "./FormFetchFuncs/Entry.js";
 
 const StartFunc = async () => {
     await StartFuncEntry();
-
 };
 
 export { StartFunc }
