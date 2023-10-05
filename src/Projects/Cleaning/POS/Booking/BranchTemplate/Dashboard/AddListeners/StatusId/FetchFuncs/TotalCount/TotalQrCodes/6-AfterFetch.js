76a3c1f5156f0c79b62ea7f665bf91c495886ce3
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-10-05 19:44:47 +0530

    SREENIVAS

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/AddListeners/StatusId/FetchFuncs/TotalCount/TotalQrCodes/6-AfterFetch.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/AddListeners/StatusId/FetchFuncs/TotalCount/TotalQrCodes/6-AfterFetch.js
@@ -1,7 +1,8 @@
 let StartFunc = ({ inFromFetch }) => {
     let jVarLocalData = inFromFetch.JsonData;
+    let inTotalQrCodesId = Object.keys(jVarLocalData).length;
 
-    jFLocalToInnerHtmlTotalQrCodesId({ inTotalQrCodesId: jVarLocalData })
+    jFLocalToInnerHtmlTotalQrCodesId({ inTotalQrCodesId })
 };
 
 let jFLocalToInnerHtmlTotalQrCodesId = ({ inTotalQrCodesId }) => {
