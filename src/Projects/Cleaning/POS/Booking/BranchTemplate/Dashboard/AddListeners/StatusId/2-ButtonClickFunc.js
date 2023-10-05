c7f96f8281661c03bb57f146b1f4e31d07966717
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-10-05 18:12:01 +0530

    SREENIVAS

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/AddListeners/StatusId/2-ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/AddListeners/StatusId/2-ButtonClickFunc.js
@@ -5,15 +5,19 @@ import { StartFunc as StartFuncLatestOrders } from "./FetchFuncs/LatestOrders/En
 import { StartFunc as StartFuncQrCodes } from "./FetchFuncs/QrCodes/EntryFile.js";
 import { StartFunc as StartFuncLatestOrdersAfterFetch } from "./FetchFuncs/LatestOrders/6-AfterFetch.js";
 import { StartFunc as StartFuncLatestQrCodes } from "./FetchFuncs/LatestQrCodes/EntryFile.js";
+import { StartFunc as StartFuncTotalCount } from "./FetchFuncs/TotalCount/EntryFile.js";
+import { StartFunc as StartFuncTodayCount } from "./FetchFuncs/TodayCount/EntryFile.js";
 
 let StartFunc = () => {
     StartFuncNewCustomers();
     StartFuncFrequentCustomers().then();
     StartFuncTodayCustomers();
     StartFuncLatestQrCodes();
-    StartFuncLatestOrders().then(OrdersData =>{
-        StartFuncQrCodes().then(QrCodesData =>{
-            StartFuncLatestOrdersAfterFetch({inFromFetch: OrdersData, inQrCodeData: QrCodesData});
+    StartFuncTotalCount();
+    StartFuncTodayCount();
+    StartFuncLatestOrders().then(OrdersData => {
+        StartFuncQrCodes().then(QrCodesData => {
+            StartFuncLatestOrdersAfterFetch({ inFromFetch: OrdersData, inQrCodeData: QrCodesData });
         });
     });
 };
