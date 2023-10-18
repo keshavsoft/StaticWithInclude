042b6601d35a750a74b4dddd55881aae54c9e3d3
Satish Addanki <satishaddanki321@gmail.com>
2023-10-18 16:13:14 +0530

    satish

--- src/Projects/CleaningPos/POS/Booking/BranchTemplate/Dashboard/AddListeners/GoButton/ToUrl.js
+++ src/Projects/CleaningPos/POS/Booking/BranchTemplate/Dashboard/AddListeners/GoButton/ToUrl.js
@@ -1,16 +1,24 @@
-import ApiConfigJson from "../../../ApiConfig.json" assert {type: 'json'};
+// import ApiConfigJson from "../../../ApiConfig.json" assert {type: 'json'};
 
-let StartFunc = ({ inCustomerName, inCustomerMobile}) => {
-    let inBranchName = ApiConfigJson.BranchName;
-    let jVarLocalToUrl = `/Cleaning/POS/Booking/${inBranchName}/NewOrder/NewOrder.html`;
-    let jVarLocalAddNew = "true";
+let StartFunc = ({ inCustomerName, inCustomerMobile }) => {
+    //     let inBranchName = ApiConfigJson.BranchName;
+    //     let jVarLocalToUrl = `/Cleaning/POS/Booking/${inBranchName}/NewOrder/NewOrder.html`;
+    //     let jVarLocalAddNew = "true";
 
-    const myUrlWithParams = new URL(`${window.location.origin}${jVarLocalToUrl}`);
+    //     const myUrlWithParams = new URL(`${window.location.origin}${jVarLocalToUrl}`);
 
-    myUrlWithParams.searchParams.append("CustomerName", inCustomerName);
-    myUrlWithParams.searchParams.append("CustomerMobile", inCustomerMobile);
+    //     myUrlWithParams.searchParams.append("CustomerName", inCustomerName);
+    //     myUrlWithParams.searchParams.append("CustomerMobile", inCustomerMobile);
 
-    window.location.href = myUrlWithParams.href;
+    //     window.location.href = myUrlWithParams.href;
+
+    const url = new URL(window.location.href);
+    const params1 = new URLSearchParams(url.search);
+    params1.append("CustomerName", inCustomerName);
+    params1.append("CustomerMobile", inCustomerMobile);
+    let NewURl = new URL("../NewOrder/NewOrder.html", url);
+    const new_url = new URL(`${NewURl.href}?${params1}`);
+    window.location.href = new_url.href;
 };
 
 export { StartFunc };
