09caf179e859c0fc72594461a89c923a3b0d10f4
Satish Addanki <satishaddanki321@gmail.com>
2023-10-25 21:03:57 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/QrCodeMenu/QrCodesStatusTable/Js/FormLoad/ListenerFuncs/RefreshBSTableId/ButtonClickFunc.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/QrCodeMenu/QrCodesStatusTable/Js/FormLoad/ListenerFuncs/RefreshBSTableId/ButtonClickFunc.js
@@ -1,10 +1,21 @@
 import { StartFunc as StartFuncCheckFunc } from "./CheckFunc.js";
 import { StartFunc as StartFuncFetchFromFuncs } from "./FetchFromFuncs/Entry.js";
+import { StartFunc as StartFuncToFactoryStatusCheck } from "./FetchFromFuncs/AfterFetch/ToFactoryStatus/EntryFile.js";
 
-let StartFunc = () => {
+let StartFunc1 = () => {
     if (StartFuncCheckFunc()) {
         StartFuncFetchFromFuncs().then();
     };
+};
+
+let StartFunc = () => {
+    const promises = [
+        new Promise((resolve, reject) => setTimeout(resolve, 3000, StartFuncFetchFromFuncs())),
+        new Promise((resolve, reject) => setTimeout(resolve, 3000, StartFuncToFactoryStatusCheck()))
+    ]
+    Promise.all(promises).then((results) => {
+        console.log("results", results);
+    })
 };
 
 export { StartFunc };
