5420cd2671f313337732f806edecaa4cb4badbc3
Satish Addanki <satishaddanki321@gmail.com>
2023-11-03 14:22:00 +0530

    satish

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/ToFactory/Scan/ShowAlert/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/ToFactory/Scan/ShowAlert/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
@@ -1,9 +1,11 @@
 import { StartFunc as StartFuncBSTableURL } from "./BSTableURL/Entry.js";
 import { StartFunc as StartFuncShowAlertURL } from "./ShowAlertURL/Entry.js";
+import { StartFunc as StartFuncFactoryFixNewURL } from "./FactoryFixURL/Entry.js";
 
 const StartFunc = () => {
     StartFuncBSTableURL();
     StartFuncShowAlertURL();
+    StartFuncFactoryFixNewURL();
 };
 
 export { StartFunc };
