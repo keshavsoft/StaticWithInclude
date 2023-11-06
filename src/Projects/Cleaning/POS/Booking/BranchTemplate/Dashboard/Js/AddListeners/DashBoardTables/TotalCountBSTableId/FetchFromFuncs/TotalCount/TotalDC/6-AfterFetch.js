80b7ce3d34a5c8b720715c3ce49deb207319951d
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-11-06 14:13:45 +0530

    SREENIVAS

--- src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/Js/AddListeners/DashBoardTables/TotalCountBSTableId/FetchFromFuncs/TotalCount/TotalDC/6-AfterFetch.js
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Dashboard/Js/AddListeners/DashBoardTables/TotalCountBSTableId/FetchFromFuncs/TotalCount/TotalDC/6-AfterFetch.js
@@ -1,13 +1,15 @@
 let StartFunc = ({ inFromFetch }) => {
     let jVarLocalData = inFromFetch.JsonData;
+    let inTotalQrCodesId = Object.keys(jVarLocalData).length;
 
-    jFLocalToInnerHtmlTotalDCId({ inTotalDCId: jVarLocalData })
+
+    jFLocalToInnerHtmlTotalDCId({ inTotalDCId: inTotalQrCodesId })
 };
 
 let jFLocalToInnerHtmlTotalDCId = ({ inTotalDCId }) => {
     let jVarLocalHtmlId = 'TotalDCId';
-   let jVarLocalTotalDCId = document.getElementById(jVarLocalHtmlId);
-   jVarLocalTotalDCId.innerHTML = inTotalDCId;
+    let jVarLocalTotalDCId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalTotalDCId.innerHTML = inTotalDCId;
 };
 
 export { StartFunc };
