1bc047d6a05bedabb24be39de06681d5fa52bc96
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-11-17 21:09:34 +0530

    SREENIVAS

---
+++ src/Projects/ForDashboards/Cleaning/Branchwise/pages/dashboard/Js/FoamLoad/EntryFile.js
@@ -0,0 +1,8 @@
+import { StartFunc as StartFuncTotalCount } from "./TotalCount/EntryFile.js";
+
+let StartFunc = async () => {
+
+    StartFuncTotalCount();
+};
+
+export { StartFunc };
---
+++ src/Projects/ForDashboards/Cleaning/Branchwise/pages/dashboard/Js/FoamLoad/TotalCount/TotalQrCodes/4-PreparePostData.js
@@ -0,0 +1,12 @@
+// import ApiConfig from "../../../../../../../../ApiConfig.json" assert{type: 'json'};
+
+const StartFunc = () => {
+    let jVarLocalBodyData = {};
+    jVarLocalBodyData.FolderName = "QrCodes";
+    jVarLocalBodyData.FileNameOnly = "Generate";
+    jVarLocalBodyData.ItemName = "Barcodes";
+
+    return jVarLocalBodyData;
+};
+
+export { StartFunc };
