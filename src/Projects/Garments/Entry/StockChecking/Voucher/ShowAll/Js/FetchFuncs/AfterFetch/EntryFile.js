8633236c8debf3996b8b059b5cefdfa25967414e
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-11-22 11:39:29 +0530

    SREENIVAS

--- src/Projects/Garments/Entry/StockChecking/Voucher/ShowAll/Js/FetchFuncs/AfterFetch/EntryFile.js
+++ src/Projects/Garments/Entry/StockChecking/Voucher/ShowAll/Js/FetchFuncs/AfterFetch/EntryFile.js
@@ -1,15 +1,15 @@
 import { StartFunc as StartFuncItemDetails } from "../../FormLoad/ItemDetails/EntryFile.js";
 
 let StartFunc = async ({ inDataToShow }) => {
-    console.log("inDataToShow::",inDataToShow);
+    jFLocalHideSpinner();
     let jVarLocalDataToShow = jFLocalToArray({ inDataToShow });
     jVarGlobalPresentViewData = await StartFuncItemDetails();
-    console.log("jVarGlobalPresentViewData:",jVarGlobalPresentViewData);
-    jFLocalHideSpinner();
+    let LocalTimeSetData = jFLocalShowDateDiffInMinSec({ inData: jVarLocalDataToShow });
+
 
     var $table = $('#table');
     $table.bootstrapTable({
-        data: jVarLocalDataToShow,
+        data: LocalTimeSetData,
     });
 };
 
@@ -31,6 +31,32 @@ let jFLocalToArray = ({ inDataToShow }) => {
     );
 
     return jVarLocalArray;
+};
+
+let jFLocalShowDateDiffInMinSec = ({ inData }) => {
+    let jVarLocalReturnArray = [];
+
+    jVarLocalReturnArray = inData.map(element => {
+
+        element.SentInterVal = jFLocalKInterval({ inCurrentdateandtime: element.DateTime });
+
+        return element;
+    });
+    return jVarLocalReturnArray;
+};
+
+let jFLocalKInterval = ({ inCurrentdateandtime }) => {
+    if (inCurrentdateandtime === undefined) {
+        return "";
+    }
+    var diffMs = (new Date() - new Date(inCurrentdateandtime)); // milliseconds between now & Christmas
+    var diffDays = Math.floor(diffMs / 86400000); // days
+    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+
+    return diffDays + " days, " + diffHrs + " hours, " + diffMins + " min...";
+
+    // console.log(diffDays + " days, " + diffHrs + " hours, " + diffMins + " min...");
 };
 
 
