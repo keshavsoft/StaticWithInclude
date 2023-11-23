cc7af82be1f8a74d06c778afe524c05c742224f7
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-11-23 20:29:07 +0530

    SREENIVAS

---
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Customer/UsersShow/Js/ShowOnDom.js
@@ -0,0 +1,8 @@
+
+let StartFunc =  () => {
+    let jVarLocalHtmlId = 'RefreshBSTableId';
+    let jVarLocalRefreshBSTableId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalRefreshBSTableId.click();
+};
+
+export { StartFunc }
---
+++ src/Projects/Cleaning/POS/Booking/BranchTemplate/Customer/UsersShow/Js/UsersShow.js
@@ -0,0 +1,15 @@
+import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
+import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
+import { StartFunc as StartFuncAdminData } from "./AdminData/StartFunc.js";
+
+const StartFunc = () => {
+    StartFuncFormLoad();
+
+    let jVarLocalFromAdmin = StartFuncAdminData({ inFormLoadFuncToRun: StartFuncShowOnDom });
+
+    if (jVarLocalFromAdmin) {
+        StartFuncShowOnDom();
+    };
+};
+
+StartFunc();
