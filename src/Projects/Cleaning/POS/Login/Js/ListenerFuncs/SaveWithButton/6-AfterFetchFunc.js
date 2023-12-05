72143f92cb8c59ad6104d02512aa2d47e635ec2c
Keshav <nknnkumar@yahoo.com>
2023-12-06 01:51:12 +0530

    keshav

--- src/Projects/Cleaning/POS/Login/Js/ListenerFuncs/SaveWithButton/6-AfterFetchFunc.js
+++ src/Projects/Cleaning/POS/Login/Js/ListenerFuncs/SaveWithButton/6-AfterFetchFunc.js
@@ -1,13 +1,31 @@
 let StartFunc = ({ inFetchData, inUserName }) => {
     localStorage.setItem("kUserName", inUserName);
 
-    if (inFetchData.KTF) {
+    if ("UserNameFound" in inFetchData) {
 
-        window.location = `/Cleaning/POS/Booking/Kakinada/Dashboard/Dashboard.html?UserName=${inUserName}&FromCreate=true`;
+        if (inFetchData.UserNameFound) {
+            //     window.location = `/Cleaning/POS/Booking/BranchTemplate/Dashboard/Dashboard.html?BranchName=Kakinada`;
+        };
+        //        "http://localhost:4119/Cleaning/POS/Booking/BranchTemplate/Dashboard/Dashboard.html#"
+    };
+
+    if ("UserAndDataPkFolderFound" in inFetchData) {
+        if (inFetchData.UserAndDataPkFolderFound) {
+            if ("RedirectPage" in inFetchData) {
+                window.location = inFetchData.RedirectPage;
+            };
+        };
+    };
+
+    if (inFetchData.KTF) {
+        if ("RedirectPage" in inFetchData) {
+            window.location = inFetchData.RedirectPage;
+        };
+        //     window.location = `/Cleaning/POS/Booking/Kakinada/Dashboard/Dashboard.html?UserName=${inUserName}&FromCreate=true`;
 
     } else {
         if (inFetchData.KTF === false && inFetchData.UserAndDataPkFolderFound && "kPK" in inFetchData) {
-            window.location = "/Cleaning/POS/Booking/Kakinada/Dashboard/Dashboard.html";
+            //       window.location = "/Cleaning/POS/Booking/Kakinada/Dashboard/Dashboard.html";
         };
     };
 };
