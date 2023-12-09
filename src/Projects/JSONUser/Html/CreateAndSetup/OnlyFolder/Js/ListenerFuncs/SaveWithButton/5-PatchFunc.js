0bebe2562a2a29818023ffad060c577724552a27
Keshav <nknnkumar@yahoo.com>
2023-12-09 22:15:50 +0530

    keshav

---
+++ src/Projects/JSONUser/Html/CreateAndSetup/OnlyFolder/Js/ListenerFuncs/SaveWithButton/5-PatchFunc.js
@@ -0,0 +1,29 @@
+import { StartFunc as StartFuncAfterFetchFunc } from "./6-AfterFetchFunc.js";
+
+
+const StartFunc = async ({ inFetchBody,inSubRoute }) => {
+
+    let jFetchUrl = `/${inSubRoute}/Users/Api/CreateAndSetup/ForChat/ForChatCredentials`;
+
+    let jLocalFetchConfig = {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inFetchBody.FormData)
+    };
+
+    let response = await fetch(jFetchUrl, jLocalFetchConfig);
+
+    let jVarLocalResponseData = await response.json();
+
+
+
+    StartFuncAfterFetchFunc({ inFetchData: jVarLocalResponseData });
+
+    // return await response.status;
+
+}
+
+export { StartFunc };
