14aaf99125961a0c94b111e2b92f654fccf68d1d
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-12-13 18:19:04 +0530

    SREENIVAS

---
+++ src/Projects/JSONUser/Html/BoilerPlate/FoldersOnly/BsTable/Js/ShowOnDom/AfterFetch/EntryFile.js
@@ -0,0 +1,61 @@
+let StartFunc = async ({ inDataToShow }) => {
+    // jFLocalHideSpinner();
+    let jVarLocalDataToShow = jFLocalToArray({ inDataToShow });
+    console.log("jVarLocalDataToShow:",jVarLocalDataToShow);
+    // let LocalTimeSetData = jFLocalShowDateDiffInMinSec({ inData: jVarLocalDataToShow });
+
+
+    var $table = $('#table');
+    $table.bootstrapTable({
+        data: jVarLocalDataToShow,
+    });
+};
+
+let jFLocalHideSpinner = () => {
+    let jVarLocalSpinnerId = document.getElementById("SpinnerId");
+    jVarLocalSpinnerId.style.display = "none";
+
+};
+
+let jFLocalToArray = ({ inDataToShow }) => {
+    let jVarLocalArray = [];
+    Object.entries(inDataToShow).forEach(
+        ([key, value]) => {
+            jVarLocalArray.push({
+                ...value,
+                pk: key
+            });
+        }
+    );
+
+    return jVarLocalArray;
+};
+
+let jFLocalShowDateDiffInMinSec = ({ inData }) => {
+    let jVarLocalReturnArray = [];
+
+    jVarLocalReturnArray = inData.map(element => {
+
+        element.SentInterVal = jFLocalKInterval({ inCurrentdateandtime: element.DateTime });
+
+        return element;
+    });
+    return jVarLocalReturnArray;
+};
+
+let jFLocalKInterval = ({ inCurrentdateandtime }) => {
+    if (inCurrentdateandtime === undefined) {
+        return "";
+    }
+    var diffMs = (new Date() - new Date(inCurrentdateandtime)); // milliseconds between now & Christmas
+    var diffDays = Math.floor(diffMs / 86400000); // days
+    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+
+    return diffDays + " days, " + diffHrs + " hours, " + diffMins + " min...";
+
+    // console.log(diffDays + " days, " + diffHrs + " hours, " + diffMins + " min...");
+};
+
+
+export { StartFunc }
