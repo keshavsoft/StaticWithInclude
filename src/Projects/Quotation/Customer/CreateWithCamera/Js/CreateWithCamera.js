be3339a0cd44bcf59be6d9ac7c4d402cfbc62c10
Keshav <nknnkumar@yahoo.com>
2023-12-15 18:00:25 +0530

    keshav

---
+++ src/Projects/Quotation/Customer/CreateWithCamera/Js/CreateWithCamera.js
@@ -0,0 +1,15 @@
+import { StartFunc as StartFuncAddlisteners } from "./Addlisteners/StartFunc.js";
+import { StartFunc as StartFuncAdminData } from "./AdminData/StartFunc.js";
+import { StartFunc as StartFuncFormLoad } from "./FormLoad/EntryFile.js";
+
+const StartFunc = () => {
+    // let jVarLocalFromAdmin = true;
+    let jVarLocalFromAdmin = StartFuncAdminData({ inFormLoadFuncToRun: StartFuncAddlisteners });
+
+    if (jVarLocalFromAdmin) {
+        StartFuncFormLoad();
+        StartFuncAddlisteners();
+    };
+};
+
+StartFunc();
