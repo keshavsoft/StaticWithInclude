40fbd3d642dbbc16c5dae6788968b4a4f124f6cc
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-12-16 17:56:18 +0530

    keshav

---
+++ ForMaguva.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+let CommonForGarments = require("./kcode/ForGarments/ForWin32");
+
+let CommonOsName = os.platform();
+
+switch (CommonOsName) {
+    case "win32":
+        CommonForGarments.StartFunc();
+
+        break;
+
+    case "linux":
+        // CommonForLinux.StartFunc();
+        break;
+
+    default:
+        break;
+};
---
+++ kcode/ForMaguva/ForWin32.js
@@ -0,0 +1,13 @@
+let CommonProjectName = "Maguva";
+
+let LocalFolderPath = `src\\Projects\\${CommonProjectName}`;
+let LocalDestinationPath = `public\\Projects\\${CommonProjectName}`;
+
+let CommonWalk = require("./WalkFuncs");
+
+let StartFunc = () => {
+    CommonWalk.walk(LocalFolderPath, LocalFolderPath, LocalDestinationPath, CommonWalk.CallBackFunc);
+};
+
+module.exports = { StartFunc };
+
