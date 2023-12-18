527f33017ecbba84fb919700045e0e2479e15ec3
Keshav <nknnkumar@yahoo.com>
2023-12-18 10:23:12 +0530

    keshav

--- src/Projects/Quotation--/Customer/Create/Js/Addlisteners/StartFunc.js
+++
@@ -1,7 +0,0 @@
-import { StartFunc as StartFuncCreateCustomer } from "./CreateCustomer/StartFunc.js";
-
-let StartFunc = () => {
-    StartFuncCreateCustomer();
-};
-
-export { StartFunc };
--- src/Projects/Quotation--/Customer/CreateCamera/Js/Addlisteners/StartFunc.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncCreateCustomer } from "./CreateCustomer/StartFunc.js";
-import { StartFunc as StartFuncImageShowButon } from "./ImageShowButon/StartFunc.js";
-
-let StartFunc = () => {
-    StartFuncCreateCustomer();
-    StartFuncImageShowButon();
-};
-
-export { StartFunc };
